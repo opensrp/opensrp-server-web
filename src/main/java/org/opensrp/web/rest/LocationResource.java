a1359f4e386a9f492607c412a91aab9e083c197e
Samuel Githengi <samuel.githengi@gmail.com>
2018-11-16 17:15:36 +0300

    Add Location Resource

---
+++ src/main/java/org/opensrp/web/rest/LocationResource.java
@@ -0,0 +1,121 @@
+package org.opensrp.web.rest;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.opensrp.common.AllConstants.BaseEntity;
+import org.opensrp.domain.PhysicalLocation;
+import org.opensrp.service.PhysicalLocationService;
+import org.opensrp.util.TaskDateTimeTypeConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+
+@Controller
+@RequestMapping(value = "/rest/location")
+public class LocationResource {
+
+	private static Logger logger = LoggerFactory.getLogger(LocationResource.class.toString());
+
+	public static Gson gson = new GsonBuilder().registerTypeAdapter(DateTime.class, new TaskDateTimeTypeConverter())
+			.serializeNulls().create();
+
+	private static final String IS_JURISDICTION = "is_jurisdiction";
+
+	private static final String PARENT_ID = "parent_id";
+
+	private static final String FALSE = "false";
+
+	private PhysicalLocationService locationService;
+
+	@Autowired
+	public void setLocationService(PhysicalLocationService locationService) {
+		this.locationService = locationService;
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<String> getByUniqueId(@PathVariable("id") String id,
+			@RequestParam(defaultValue = FALSE, required = false) boolean isJurisdiction) {
+		return new ResponseEntity<>(
+				gson.toJson(isJurisdiction ? locationService.getLocation(id) : locationService.getStructure(id)),
+				HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/sync", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<String> getLocations(@RequestParam(BaseEntity.SERVER_VERSIOIN) String serverVersion,
+			@RequestParam(value = IS_JURISDICTION, defaultValue = FALSE, required = false) boolean isJurisdiction,
+			@RequestParam(value = PARENT_ID, required = false) String parentId) {
+		long currentServerVersion = 0;
+		try {
+			currentServerVersion = Long.parseLong(serverVersion);
+		} catch (NumberFormatException e) {
+			logger.error("server version not a number");
+		}
+		if (isJurisdiction)
+			return new ResponseEntity<>(gson.toJson(locationService.findLocationsByServerVersion(currentServerVersion)),
+					HttpStatus.OK);
+		else
+			return new ResponseEntity<>(
+					gson.toJson(locationService.findStructuresByParentAndServerVersion(parentId, currentServerVersion)),
+					HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<HttpStatus> create(@RequestBody String entity,
+			@RequestParam(value = IS_JURISDICTION, defaultValue = FALSE, required = false) boolean isJurisdiction) {
+		try {
+			PhysicalLocation location = gson.fromJson(entity, PhysicalLocation.class);
+			location.setJurisdiction(isJurisdiction);
+			locationService.add(location);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (JsonSyntaxException e) {
+			logger.error("The request doesnt contain a valid task representation" + entity);
+		}
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<HttpStatus> update(@RequestBody String entity,
+			@RequestParam(value = IS_JURISDICTION, defaultValue = FALSE, required = false) boolean isJurisdiction) {
+		try {
+			PhysicalLocation location = gson.fromJson(entity, PhysicalLocation.class);
+			location.setJurisdiction(isJurisdiction);
+			locationService.update(location);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (JsonSyntaxException e) {
+			logger.error("The request doesnt contain a valid task representation" + entity);
+		}
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<HttpStatus> saveBatch(@RequestBody String entity,
+			@RequestParam(value = IS_JURISDICTION, defaultValue = FALSE, required = false) boolean isJurisdiction) {
+		try {
+			Type listType = new TypeToken<List<PhysicalLocation>>() {
+			}.getType();
+			List<PhysicalLocation> locations = gson.fromJson(entity, listType);
+			locationService.saveLocations(locations, isJurisdiction);
+			return new ResponseEntity<>(HttpStatus.CREATED);
+		} catch (JsonSyntaxException e) {
+			logger.error("The request doesnt contain a valid task representation" + entity);
+		}
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
