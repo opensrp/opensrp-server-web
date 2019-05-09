3154c2c025f85ddecb721cdbbe7402d38dc4caea
Samuel Githengi <samuel.githengi@gmail.com>
2019-05-10 00:15:05 +0300

    Allow CORS

--- src/main/java/org/opensrp/web/HttpHeaderFactory.java
+++ src/main/java/org/opensrp/web/HttpHeaderFactory.java
@@ -4,9 +4,17 @@ import org.opensrp.common.AllConstants;
 import org.springframework.http.HttpHeaders;
 
 public class HttpHeaderFactory {
-    public static HttpHeaders allowOrigin(String origin) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(AllConstants.HTTP.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, origin);
-        return headers;
-    }
+
+	private static final String ACCESS_CONTROL_ALLOW_ORIGIN_METHODS = "Access-Control-Allow-Methods";
+	private static final String ACCESS_CONTROL_ALLOW_ORIGIN_HEADERS = "Access-Control-Allow-Header";
+	private static final String ACCESS_CONTROL_ALLOW_MAX_AGE = "Access-Control-Max-Age";
+
+	public static HttpHeaders allowOrigin(String origin) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(AllConstants.HTTP.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, origin);
+		headers.add(ACCESS_CONTROL_ALLOW_ORIGIN_METHODS, "GET, POST");
+		headers.add(ACCESS_CONTROL_ALLOW_ORIGIN_HEADERS, "origin, content-type, accept, x-requested-with");
+		headers.add(ACCESS_CONTROL_ALLOW_MAX_AGE, "60");// 1 min
+		return headers;
+	}
 }
