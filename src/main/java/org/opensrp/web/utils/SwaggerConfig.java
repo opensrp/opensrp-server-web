59ceb9189c7bf4e1172c9931582c66e0b389820e
richard <rkareko@ona.io>
2019-06-28 11:37:19 +0300

    Initial setup

---
+++ src/main/java/org/opensrp/web/utils/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package org.opensrp.web.utils;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableWebMvc
+@EnableSwagger
+//@ComponentScan("org.opensrp.web")
+public class SwaggerConfig {
+
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    /**
+     * Required to autowire SpringSwaggerConfig
+     */
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    /**
+     * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc framework - allowing for multiple
+     * swagger groups i.e. same code base multiple swagger resource listings.
+     */
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation(){
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                .includePatterns(".*pet.*");
+    }
+
+}
