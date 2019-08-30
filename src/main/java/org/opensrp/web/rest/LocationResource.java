f8f53c744699988ce7cbe4dee5936eda76687074
richard <rkareko@ona.io>
2019-08-30 17:01:18 +0300

    Get all children and grand children of a jurisdiction

--- src/main/java/org/opensrp/web/rest/LocationResource.java
+++ src/main/java/org/opensrp/web/rest/LocationResource.java
@@ -62,6 +62,8 @@ public class LocationResource {
 
     public static final String JURISDICTION_IDS = "jurisdiction_ids";
 
+	public static final String JURISDICTION_ID = "jurisdiction_id";
+
 	private PhysicalLocationService locationService;
 
 	@Autowired
@@ -267,5 +269,28 @@ public class LocationResource {
         }
 
     }
+
+	/**
+	 * This methods provides an API endpoint that searches for a location and it's children using the provided location id
+	 * It returns the Geometry optionally if @param returnGeometry is set to true.
+	 * @param returnGeometry boolean which controls if geometry is returned
+	 * @param jurisdictionId location id
+	 * @return location together with it's children whose id matches the provided param
+	 */
+	@RequestMapping(value = "/findByIdWithChildren", method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<String> findByIdWithChildren(
+			@RequestParam(value = RETURN_GEOMETRY, defaultValue = FALSE, required = false) boolean returnGeometry,
+			@RequestParam(value = JURISDICTION_ID, required = false) String jurisdictionId) {
+
+		try {
+			return new ResponseEntity<>(
+					gson.toJson(locationService.findLocationByIdWithChildren(returnGeometry, jurisdictionId)), HttpStatus.OK);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
 
 }
