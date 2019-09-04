357e0dfe0c36562523024b01a935b6608f6648e5
richard <rkareko@ona.io>
2019-09-04 14:52:04 +0300

    Add page size param to endpoint

--- src/main/java/org/opensrp/web/rest/LocationResource.java
+++ src/main/java/org/opensrp/web/rest/LocationResource.java
@@ -64,6 +64,10 @@ public class LocationResource {
 
 	public static final String JURISDICTION_ID = "jurisdiction_id";
 
+	public static final String PAGE_SIZE = "page_size";
+
+	public static final String DEFAULT_PAGE_SIZE = "1000";
+
 	private PhysicalLocationService locationService;
 
 	@Autowired
@@ -275,17 +279,19 @@ public class LocationResource {
 	 * It returns the Geometry optionally if @param returnGeometry is set to true.
 	 * @param returnGeometry boolean which controls if geometry is returned
 	 * @param jurisdictionId location id
+	 * @param pageSize number of records to be returned
 	 * @return location together with it's children whose id matches the provided param
 	 */
 	@RequestMapping(value = "/findByIdWithChildren", method = RequestMethod.GET, produces = {
 			MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<String> findByIdWithChildren(
 			@RequestParam(value = RETURN_GEOMETRY, defaultValue = FALSE, required = false) boolean returnGeometry,
+			@RequestParam(value = PAGE_SIZE, defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize,
 			@RequestParam(value = JURISDICTION_ID, required = false) String jurisdictionId) {
 
 		try {
 			return new ResponseEntity<>(
-					gson.toJson(locationService.findLocationByIdWithChildren(returnGeometry, jurisdictionId)), HttpStatus.OK);
+					gson.toJson(locationService.findLocationByIdWithChildren(returnGeometry, jurisdictionId, pageSize)), HttpStatus.OK);
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
