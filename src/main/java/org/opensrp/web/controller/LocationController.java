a8aca8ec85c5fc775435c8924497f9c33f8a52db
cozej4 <ilakozejumanne@gmail.com>
2020-02-19 21:36:00 +0300

    Code refactoring

--- src/main/java/org/opensrp/web/controller/LocationController.java
+++ src/main/java/org/opensrp/web/controller/LocationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.google.gson.Gson;
 
