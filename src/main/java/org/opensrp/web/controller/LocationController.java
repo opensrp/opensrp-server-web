aff5f267a3750cd82fff8ce22e6035db1ac941ad
cozej4 <ilakozejumanne@gmail.com>
2020-02-19 16:57:50 +0300

    Implemented endpoint for gettting location by level and tags

--- src/main/java/org/opensrp/web/controller/LocationController.java
+++ src/main/java/org/opensrp/web/controller/LocationController.java
@@ -1,6 +1,7 @@
 package org.opensrp.web.controller;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.opensrp.connector.openmrs.service.OpenmrsLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.google.gson.Gson;
 
@@ -33,5 +35,28 @@ public class LocationController {
 	@ResponseBody
 	public ResponseEntity<String> getLocationTree(@PathVariable("uuid") String uuid) throws JSONException {
 		return new ResponseEntity<>(new Gson().toJson(openmrsLocationService.getLocationTreeOf(uuid)),HttpStatus.OK);
+	}
+
+	/**
+	 * This method receives the uuid of a location within the location hierarchy level
+	 * the tag name of top location hierarchy level to query locations from
+	 * and a list of location tags to be returned
+	 *  return a list of all other location within the location hierarchy level matching the requested tags
+	 * @param  jsonObject object containing
+	 *              locationUUID ,string of any location within the hierarchy level within the a hierarchy,
+	 *              locationTopLevel, string of the tag name of top location hierarchy level to query locations from,
+	 *              locationTagsQueried, a jsonArray of containing tags of locations to be returned
+	 * @return List of all facilities within the same hierarchy level.
+	 */
+
+	@RequestMapping(headers = {"Accept=application/json"}, method = RequestMethod.POST, value = "/getLocationsByLevelAndTags")
+	public ResponseEntity<String> getLocationsWithinALevelAndTags(@RequestBody JSONObject jsonObject) throws JSONException {
+		return new ResponseEntity<>(new Gson().toJson(
+				openmrsLocationService.getLocationsByLevelAndTags(
+						jsonObject.getString("locationUUID"),
+						jsonObject.getString("locationTopLevel"),
+						jsonObject.getJSONArray("locationTagsQueried")
+				)
+		), HttpStatus.OK);
 	}
 }
