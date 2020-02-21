0f8e1d77d8c1b1ff4df91d5b4c8771dbd4733599
Samuel Githengi <samuel.githengi@gmail.com>
2020-02-21 17:38:08 +0300

    Upgrade testing library

--- src/test/java/org/opensrp/web/rest/PlanResourceTest.java
+++ src/test/java/org/opensrp/web/rest/PlanResourceTest.java
@@ -24,7 +24,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
