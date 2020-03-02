f926ad1a9be23d4df3f44f2cefd56a0ef08032f5
cozej4 <ilakozejumanne@gmail.com>
2020-03-02 16:34:36 +0300

    Code cleaning

--- src/main/java/org/opensrp/web/rest/EventResource.java
+++ src/main/java/org/opensrp/web/rest/EventResource.java
@@ -3,7 +3,6 @@ package org.opensrp.web.rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.mysql.jdbc.StringUtils;
 import java.text.ParseException;
