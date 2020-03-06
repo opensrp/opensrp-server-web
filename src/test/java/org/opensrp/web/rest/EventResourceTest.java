7c5b79f29f2292a162633b3e1911f0d1e76517ee
Elly Kitoto <junkmailstoelly@gmail.com>
2020-03-06 15:07:29 +0300

    Fix codacy issue
    Remove generic imports

--- src/test/java/org/opensrp/web/rest/EventResourceTest.java
+++ src/test/java/org/opensrp/web/rest/EventResourceTest.java
@@ -1,7 +1,20 @@
 package org.opensrp.web.rest;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.opensrp.web.Constants.DEFAULT_GET_ALL_IDS_LIMIT;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.util.*;
+
 import org.apache.commons.lang3.tuple.Pair;
 import org.json.JSONArray;
 import org.junit.Before;
@@ -14,14 +27,8 @@ import org.opensrp.service.EventService;
 import org.opensrp.web.bean.Identifier;
 import org.springframework.http.ResponseEntity;
 
-import java.io.IOException;
-import java.util.*;
-
-import static junit.framework.Assert.assertFalse;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-import static org.opensrp.web.Constants.DEFAULT_GET_ALL_IDS_LIMIT;
-import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class EventResourceTest extends BaseResourceTest<Event> {
 
