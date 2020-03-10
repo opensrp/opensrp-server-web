bffaf2b3e9fdf5f7c5d138b03e6687b7e56a53b2
Samuel Githengi <samuel.githengi@gmail.com>
2020-03-10 14:55:57 +0300

    Move whitelist of static files to java configuration

--- src/main/java/org/opensrp/web/config/oauth/ResourceServerConfig.java
+++ src/main/java/org/opensrp/web/config/oauth/ResourceServerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,6 +28,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.mvcMatchers("/rest/**").hasRole(Role.OPENMRS)
+		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
@@ -34,6 +36,13 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("test").password(passwordEncoder().encode("test123")).roles(Role.OPENMRS);
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().mvcMatchers("/js/**")
+		.and().ignoring().mvcMatchers("/css/**")
+		.and().ignoring().mvcMatchers("/images/**");
 	}
 	
 	@Bean
