b0e33ebf35e9057a90b4c3ecc36f81b9aebc437c
richard <rkareko@ona.io>
2020-03-19 16:20:26 +0300

    Update SyncClientsAndEventsByBaseEntityIds test

--- src/test/java/org/opensrp/web/rest/shadow/EventResourceShadow.java
+++ src/test/java/org/opensrp/web/rest/shadow/EventResourceShadow.java
@@ -6,16 +6,17 @@ import org.opensrp.service.EventService;
 import org.opensrp.web.rest.EventResource;
 
 public class EventResourceShadow extends EventResource {
-
-    public EventResourceShadow(ClientService clientService, EventService eventService) {
-        super(clientService, eventService);
-    }
-
-    public EventResourceShadow() {
-        super(null, null);
-    }
-
-    public void setObjectMapper(ObjectMapper objectMapper) {
-        super.setObjectMapper(objectMapper);
-    }
+	
+	public EventResourceShadow(ClientService clientService, EventService eventService) {
+		super(clientService, eventService);
+	}
+	
+	public EventResourceShadow() {
+		super(null, null);
+	}
+	
+	@Override
+	public void setObjectMapper(ObjectMapper objectMapper) {
+		super.setObjectMapper(objectMapper);
+	}
 }
