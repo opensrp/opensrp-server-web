39ee364261e2d27bfe2ccce216eb9895240ab193
rehammuzzamil <62061579+rehammuzzamil@users.noreply.github.com>
2020-04-16 14:33:34 +0500

    211 : Add missing unit tests

---
+++ src/test/java/org/opensrp/web/controller/UniqueIdControllerTest.java
@@ -0,0 +1,140 @@
+package org.opensrp.web.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opensrp.api.domain.User;
+import org.opensrp.service.OpenmrsIDService;
+import org.opensrp.web.rest.it.TestWebContextLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.AssertionErrors.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = TestWebContextLoader.class, locations = { "classpath:test-webmvc-config.xml", })
+
+public class UniqueIdControllerTest {
+
+	@Autowired
+	protected WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	private UniqueIdController uniqueIdController;
+
+	@Mock
+	private OpenmrsIDService openmrsIdService;
+
+	@Mock
+	private UserController userController;
+
+	protected ObjectMapper mapper = new ObjectMapper();
+
+	private final String BASE_URL = "/uniqueids";
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup(uniqueIdController).build();
+	}
+
+	@Test
+	public void testGetOpenMRSIdentifiers() throws Exception {
+		List<String> mocked_expected_ids = new ArrayList<>();
+		mocked_expected_ids.add("1");
+		Authentication authentication = mock(Authentication.class);
+		authentication.setAuthenticated(Boolean.TRUE);
+		User user = new User("Base-entity-id");
+		user.setUsername("admin");
+		user.setPassword("admin");
+		SecurityContext securityContext = mock(SecurityContext.class);
+		SecurityContextHolder.setContext(securityContext);
+
+		when(openmrsIdService
+				.getOpenMRSIdentifiers(any(String.class), any(String.class), nullable(String.class), any(String.class)))
+				.thenReturn(mocked_expected_ids);
+		when(userController.getAuthenticationAdvisor(any(HttpServletRequest.class))).thenReturn(getMockedAuthentication());
+		when(securityContext.getAuthentication()).thenReturn(authentication);
+		when(SecurityContextHolder.getContext().getAuthentication().getPrincipal()).thenReturn(user);
+
+		MvcResult result = mockMvc.perform(get(BASE_URL + "/get").param("numberToGenerate", "10")
+				.param("source", "test"))
+				.andExpect(status().isOk()).andReturn();
+
+		String responseString = result.getResponse().getContentAsString();
+		if (responseString.isEmpty()) {
+			fail("Test case failed");
+		}
+		JsonNode actualObj = mapper.readTree(responseString);
+
+		assertEquals(actualObj.get("identifiers").get(0).asText(), "1");
+		assertEquals(actualObj.get("identifiers").size(), 1);
+	}
+
+	private Authentication getMockedAuthentication() {
+		Authentication authentication = new Authentication() {
+
+			@Override
+			public Collection<? extends GrantedAuthority> getAuthorities() {
+				return null;
+			}
+
+			@Override
+			public Object getCredentials() {
+				return "";
+			}
+
+			@Override
+			public Object getDetails() {
+				return null;
+			}
+
+			@Override
+			public Object getPrincipal() {
+				return "Test User";
+			}
+
+			@Override
+			public boolean isAuthenticated() {
+				return false;
+			}
+
+			@Override
+			public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+			}
+
+			@Override
+			public String getName() {
+				return "admin";
+			}
+		};
+
+		return authentication;
+	}
+}
