6a446381219284f8291abd7c5d122ea7c056d833
Samuel Githengi <samuel.githengi@gmail.com>
2020-04-22 12:37:58 +0300

    Configure keycloak with test realm

--- src/main/java/org/opensrp/web/config/security/SecurityConfig.java
+++ src/main/java/org/opensrp/web/config/security/SecurityConfig.java
@@ -5,9 +5,11 @@ package org.opensrp.web.config.security;
 
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.opensrp.web.config.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -33,6 +35,17 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter{
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+    
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http
+                .authorizeRequests()
+                .antMatchers("/rest/**").hasRole(Role.OPENMRS)
+                .anyRequest().permitAll();
     }
 	
 }
