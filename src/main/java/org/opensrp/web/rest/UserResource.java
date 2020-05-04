cd91f376d23e4693cbbc8ccf30621a06feb9fa99
Samuel Githengi <samuel.githengi@gmail.com>
2020-05-04 20:03:59 +0300

    Handle all status code exeptions

--- src/main/java/org/opensrp/web/rest/UserResource.java
+++ src/main/java/org/opensrp/web/rest/UserResource.java
@@ -12,8 +12,6 @@ import org.opensrp.connector.openmrs.service.OpenmrsUserService;
 import org.opensrp.web.bean.ResetPasswordBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 
 /**
  * @author Samuel Githengi created on 10/09/19
@@ -65,11 +63,11 @@ public class UserResource {
 		    keycloakDeployment.getRealm());
 		ResponseEntity<String> response = null;
 		try {
-			//response=restTemplate.exchange(url, HttpMethod.POST,new HttpEntity<ResetPasswordBean>(resetPasswordBean), String.class);
 			response = restTemplate.postForEntity(url, resetPasswordBean, String.class);
 		}
-		catch (HttpClientErrorException.BadRequest e) {
-			return new ResponseEntity<String>(e.getResponseBodyAsString(), HttpStatus.BAD_REQUEST);
+		catch (HttpStatusCodeException e) {
+			return new ResponseEntity<String>(e.getResponseBodyAsString(), e.getStatusCode());
+			
 		}
 		return response;
 	}
