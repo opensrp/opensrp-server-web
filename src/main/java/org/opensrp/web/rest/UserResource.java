bcb96d5e4aa28f6f4cdd4a643ed5ea63ea25900c
Samuel Githengi <samuel.githengi@gmail.com>
2020-05-04 19:49:34 +0300

    return bad request if keycload return bad request

--- src/main/java/org/opensrp/web/rest/UserResource.java
+++ src/main/java/org/opensrp/web/rest/UserResource.java
@@ -12,6 +12,8 @@ import org.opensrp.connector.openmrs.service.OpenmrsUserService;
 import org.opensrp.web.bean.ResetPasswordBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
 
 /**
  * @author Samuel Githengi created on 10/09/19
@@ -60,7 +63,15 @@ public class UserResource {
 	private ResponseEntity<String> changePassword(@RequestBody ResetPasswordBean resetPasswordBean) {
 		String url = MessageFormat.format(resetPasswordURL, keycloakDeployment.getAuthServerBaseUrl(),
 		    keycloakDeployment.getRealm());
-		return restTemplate.postForEntity(url, resetPasswordBean, String.class);
+		ResponseEntity<String> response = null;
+		try {
+			//response=restTemplate.exchange(url, HttpMethod.POST,new HttpEntity<ResetPasswordBean>(resetPasswordBean), String.class);
+			response = restTemplate.postForEntity(url, resetPasswordBean, String.class);
+		}
+		catch (HttpClientErrorException.BadRequest e) {
+			return new ResponseEntity<String>(e.getResponseBodyAsString(), HttpStatus.BAD_REQUEST);
+		}
+		return response;
 	}
 	
 }
