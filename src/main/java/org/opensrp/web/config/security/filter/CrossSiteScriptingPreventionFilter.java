c6af0882d2fefc1011d7e0e2864134a533ab3b89
rehammuzzamil <62061579+rehammuzzamil@users.noreply.github.com>
2020-05-12 21:19:35 +0500

    271 : Changed approach to encode request body

--- src/main/java/org/opensrp/web/config/security/filter/CrossSiteScriptingPreventionFilter.java
+++ src/main/java/org/opensrp/web/config/security/filter/CrossSiteScriptingPreventionFilter.java
@@ -1,16 +1,9 @@
 package org.opensrp.web.config.security.filter;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.io.IOUtils;
-import org.opensrp.common.audit.Auditor;
-import org.owasp.encoder.Encode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 
 import javax.servlet.FilterChain;
@@ -21,14 +14,10 @@ import javax.servlet.ServletException;
 import javax.servlet.Filter;
 import javax.servlet.FilterConfig;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
 
 public class CrossSiteScriptingPreventionFilter implements Filter {
 
 	private static Logger logger = LoggerFactory.getLogger(CrossSiteScriptingPreventionFilter.class);
-	private static ObjectMapper mapper = new ObjectMapper();
-	private Auditor auditor;
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -40,9 +29,17 @@ public class CrossSiteScriptingPreventionFilter implements Filter {
 		logger.debug("Inside CrossSiteScriptingPreventionFilter  ...............");
 		XssPreventionRequestWrapper wrappedRequest = new XssPreventionRequestWrapper(
 				(HttpServletRequest) request);
-		String body = IOUtils.toString(wrappedRequest.getReader());
-		wrappedRequest.resetInputStream();
-		chain.doFilter(wrappedRequest, response);
+		
+		if((wrappedRequest.getMethod().equals(HttpMethod.POST) || wrappedRequest.getMethod().equals(HttpMethod.PUT))
+				&& !request.getContentType().contains(MediaType.MULTIPART_FORM_DATA_VALUE)) {
+			String body = IOUtils.toString(wrappedRequest.getReader());
+			wrappedRequest.resetInputStream();
+			chain.doFilter(wrappedRequest, response);
+		}
+		else {
+			chain.doFilter(request,response);
+		}
+		
 	}
 
 	@Override
