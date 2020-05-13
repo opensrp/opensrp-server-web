211369fa0b61220a24062b4f72fc5e6ed5e7b97e
Samuel Githengi <samuel.githengi@gmail.com>
2020-05-13 13:17:13 +0300

    add unit tests for reset password

--- src/main/java/org/opensrp/web/bean/ResetPasswordBean.java
+++ src/main/java/org/opensrp/web/bean/ResetPasswordBean.java
@@ -3,10 +3,17 @@
  */
 package org.opensrp.web.bean;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * @author Samuel Githengi created on 05/04/20
  */
 
+@NoArgsConstructor
+@Setter
+@Getter
 public class ResetPasswordBean {
 	
 	private String currentPassword;
@@ -14,29 +21,5 @@ public class ResetPasswordBean {
 	private String newPassword;
 	
 	private String confirmation;
-	
-	public String getCurrentPassword() {
-		return currentPassword;
-	}
-	
-	public void setCurrentPassword(String currentPassword) {
-		this.currentPassword = currentPassword;
-	}
-	
-	public String getNewPassword() {
-		return newPassword;
-	}
-	
-	public void setNewPassword(String newPassword) {
-		this.newPassword = newPassword;
-	}
-	
-	public String getConfirmation() {
-		return confirmation;
-	}
-	
-	public void setConfirmation(String confirmation) {
-		this.confirmation = confirmation;
-	}
 	
 }
--- src/test/java/org/opensrp/TestSecurityConfig.java
+++ src/test/java/org/opensrp/TestSecurityConfig.java
@@ -9,10 +9,8 @@ import java.io.IOException;
 
 import org.keycloak.adapters.KeycloakDeployment;
 import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 /**
  * @author Samuel Githengi created on 05/13/20
@@ -21,7 +19,6 @@ import org.springframework.context.annotation.Scope;
 public class TestSecurityConfig {
 	
 	@Bean
-	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 	public KeycloakRestTemplate keycloakRestTemplate() {
 		return mock(KeycloakRestTemplate.class);
 	}
