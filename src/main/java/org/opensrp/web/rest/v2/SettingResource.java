872bd31d55a1211f4eee4bb4a45cd0e946db4bb5
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-05-20 01:17:39 +0300

    :zap: v2 server settings addition

--- src/main/java/org/opensrp/web/rest/v2/SettingResource.java
+++ src/main/java/org/opensrp/web/rest/v2/SettingResource.java
@@ -1,5 +1,6 @@
 package org.opensrp.web.rest.v2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
@@ -8,9 +9,11 @@ import org.apache.http.util.TextUtils;
 import org.joda.time.DateTime;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.opensrp.common.AllConstants;
 import org.opensrp.common.AllConstants.BaseEntity;
 import org.opensrp.domain.setting.Setting;
 import org.opensrp.domain.setting.SettingConfiguration;
+import org.opensrp.repository.postgres.handler.BaseTypeHandler;
 import org.opensrp.repository.postgres.handler.SettingTypeHandler;
 import org.opensrp.search.SettingSearchBean;
 import org.opensrp.service.SettingService;
@@ -28,26 +31,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-
-import static java.text.MessageFormat.format;
-import static org.opensrp.common.AllConstants.Event.LOCATION_ID;
-import static org.opensrp.common.AllConstants.Event.PROVIDER_ID;
-import static org.opensrp.common.AllConstants.Event.TEAM;
-import static org.opensrp.common.AllConstants.Event.TEAM_ID;
-import static org.opensrp.web.rest.RestUtils.getStringFilter;
-
-;
 
 @Controller
 @RequestMapping (value = "/rest/v2/settings")
 public class SettingResource {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SettingResource.class.toString());
-	private final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+	public static Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 			.registerTypeAdapter(DateTime.class, new DateTimeTypeConverter()).create();
 	private SettingService settingService;
 	
@@ -74,17 +69,17 @@ public class SettingResource {
 	 * @return A list of settings
 	 */
 	@RequestMapping (value = "/", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<String> getAll(HttpServletRequest request) {
+	public ResponseEntity<String> getAllSettings(HttpServletRequest request) throws JsonProcessingException {
 		try {
 			JSONObject response = new JSONObject();
-			String serverVersion = getStringFilter(BaseEntity.SERVER_VERSIOIN, request);
-			String providerId = getStringFilter(PROVIDER_ID, request);
-			String locationId = getStringFilter(LOCATION_ID, request);
-			String team = getStringFilter(TEAM, request);
-			String teamId = getStringFilter(TEAM_ID, request);
+			String serverVersion = RestUtils.getStringFilter(BaseEntity.SERVER_VERSIOIN, request);
+			String providerId = RestUtils.getStringFilter(AllConstants.Event.PROVIDER_ID, request);
+			String locationId = RestUtils.getStringFilter(AllConstants.Event.LOCATION_ID, request);
+			String team = RestUtils.getStringFilter(AllConstants.Event.TEAM, request);
+			String teamId = RestUtils.getStringFilter(AllConstants.Event.TEAM_ID, request);
 			
 			if ((TextUtils.isBlank(team) && TextUtils.isBlank(providerId) && TextUtils.isBlank(locationId)
-					&& TextUtils.isBlank(teamId) && TextUtils.isBlank(teamId)) || TextUtils.isBlank(serverVersion)) {
+					&& TextUtils.isBlank(teamId) && TextUtils.isBlank(team)) || TextUtils.isBlank(serverVersion)) {
 				return new ResponseEntity<>(response.toString(), RestUtils.getJSONUTF8Headers(), HttpStatus.BAD_REQUEST);
 			}
 			
@@ -106,7 +101,7 @@ public class SettingResource {
 			return new ResponseEntity<>(new JSONArray(settingsArrayString).toString(), RestUtils.getJSONUTF8Headers(), HttpStatus.OK);
 			
 		} catch (Exception e) {
-			logger.error(format("Fetching settings failed with the following error {0}.- ", e));
+			logger.error(MessageFormat.format("Fetching settings failed with the following error {0}.- ", e));
 			return new ResponseEntity<>(RestUtils.getJSONUTF8Headers(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
@@ -123,17 +118,16 @@ public class SettingResource {
 		return settingList;
 	}
 	
-	@RequestMapping(method = {RequestMethod.POST,RequestMethod.PUT}, consumes = { MediaType.APPLICATION_JSON_VALUE,
-			MediaType.TEXT_PLAIN_VALUE })
-	public ResponseEntity<HttpStatus> createOrUpdate(@RequestBody String entity) {
+	@RequestMapping (method = {RequestMethod.POST, RequestMethod.PUT}, consumes = {MediaType.APPLICATION_JSON_VALUE,
+			MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> createOrUpdate(@RequestBody String entity) {
 		try {
 			Setting setting = gson.fromJson(entity, Setting.class);
-			settingService.saveSetting(convertSettingToSettingConfiguration(setting));
-			return new ResponseEntity<>(HttpStatus.CREATED);
-		}
-		catch (JsonSyntaxException e) {
+			String identifier = settingService.saveSetting(convertSettingToSettingConfiguration(setting));
+			return new ResponseEntity<>(identifier, RestUtils.getJSONUTF8Headers(), HttpStatus.CREATED);
+		} catch (JsonSyntaxException e) {
 			logger.error("The request doesnt contain a valid settings representation" + entity);
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>("", RestUtils.getJSONUTF8Headers(),HttpStatus.BAD_REQUEST);
 		}
 		
 	}
@@ -146,79 +140,6 @@ public class SettingResource {
 		settingConfiguration.setIdentifier(setting.getIdentifier());
 		settingConfiguration.setType(setting.getType());
 		
-		return  settingConfiguration.toString();
+		return settingConfiguration.toString();
 	}
-	
-	
-	/*@RequestMapping(method = RequestMethod.GET, value = "/sync")
-	public @ResponseBody ResponseEntity<String> findSettingsByVersion(HttpServletRequest request) {
-		JSONObject response = new JSONObject();
-		
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.add("Content-Type", "application/json; charset=utf-8");
-		try {
-			
-			String serverVersion = getStringFilter(BaseEntity.SERVER_VERSIOIN, request);
-			String providerId = getStringFilter(PROVIDER_ID, request);
-			String locationId = getStringFilter(LOCATION_ID, request);
-			String team = getStringFilter(TEAM, request);
-			String teamId = getStringFilter(TEAM_ID, request);
-			
-			if ((TextUtils.isBlank(team) && TextUtils.isBlank(providerId) && TextUtils.isBlank(locationId)
-			        && TextUtils.isBlank(teamId) && TextUtils.isBlank(teamId)) || TextUtils.isBlank(serverVersion)) {
-				return new ResponseEntity<>(response.toString(), responseHeaders, HttpStatus.BAD_REQUEST);
-			}
-			
-			Long lastSyncedServerVersion = serverVersion != null ? Long.valueOf(serverVersion) + 1 : 0;
-			
-			SettingSearchBean settingQueryBean = new SettingSearchBean();
-			settingQueryBean.setTeam(team);
-			settingQueryBean.setTeamId(teamId);
-			settingQueryBean.setProviderId(providerId);
-			settingQueryBean.setLocationId(locationId);
-			settingQueryBean.setServerVersion(lastSyncedServerVersion);
-			
-			List<SettingConfiguration> SettingConfigurations = settingService.findSettings(settingQueryBean);
-
-			SettingTypeHandler settingTypeHandler = new SettingTypeHandler();
-			String settingsArrayString = settingTypeHandler.mapper.writeValueAsString(SettingConfigurations);
-
-			return new ResponseEntity<>(new JSONArray(settingsArrayString).toString(), responseHeaders,
-					HttpStatus.OK);
-			
-		}
-		catch (Exception e) {
-			logger.error(format("Sync data processing failed with exception {0}.- ", e));
-			return new ResponseEntity<>(responseHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
-	}*/
-	
-	/*@RequestMapping (headers = {"Accept=application/json"}, method = POST, value = "/sync")
-	public ResponseEntity<String> saveSetting(@RequestBody String data) {
-		JSONObject response = new JSONObject();
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.add("Content-Type", "application/json; charset=utf-8");
-		
-		JSONObject syncData = new JSONObject(data);
-		
-		if (!syncData.has("settingConfigurations")) {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		} else {
-			
-			JSONArray clientSettings = syncData.getJSONArray(AllConstants.Event.SETTING_CONFIGURATIONS);
-			JSONArray dbSettingsArray = new JSONArray();
-			
-			for (int i = 0; i < clientSettings.length(); i++) {
-				
-				dbSettingsArray.put(settingService.saveSetting(clientSettings.getString(i)));
-				
-			}
-			
-			response.put("validated_records", dbSettingsArray);
-			
-		}
-		
-		return new ResponseEntity<>(response.toString(), responseHeaders, HttpStatus.OK);
-	}*/
 }
---
+++ src/test/java/org/opensrp/web/rest/v2/SettingResourceTest.java
@@ -0,0 +1,188 @@
+package org.opensrp.web.rest.v2;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.opensrp.common.AllConstants;
+import org.opensrp.domain.setting.Setting;
+import org.opensrp.domain.setting.SettingConfiguration;
+import org.opensrp.search.SettingSearchBean;
+import org.opensrp.service.SettingService;
+import org.opensrp.web.config.security.filter.CrossSiteScriptingPreventionFilter;
+import org.opensrp.web.rest.it.TestWebContextLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.AssertionErrors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@RunWith (SpringJUnit4ClassRunner.class)
+@ContextConfiguration (loader = TestWebContextLoader.class, locations = {"classpath:test-webmvc-config.xml",})
+public class SettingResourceTest {
+	
+	private final String BASE_URL = "/rest/v2/settings/";
+	private final String EXPECTED_TEAM_ID = "TEAM-ID-123";
+	@Autowired
+	protected WebApplicationContext webApplicationContext;
+	protected ObjectMapper mapper = new ObjectMapper().enableDefaultTyping();
+	@Mock
+	private SettingService settingService;
+	@InjectMocks
+	private SettingResource settingResource;
+	private List<SettingConfiguration> listSettingConfigurations;
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		listSettingConfigurations = new ArrayList<>();
+		SettingConfiguration settingConfiguration = new SettingConfiguration();
+		settingConfiguration.setIdentifier("site_characteristics");
+		settingConfiguration.setTeamId("my-team-id");
+		listSettingConfigurations.add(settingConfiguration);
+		mockMvc = org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup(settingResource).
+				addFilter(new CrossSiteScriptingPreventionFilter(), "/*").build();
+	}
+	
+	@Test
+	public void testGetByUniqueId() throws Exception {
+		List<Setting> settingList = new ArrayList<>();
+		Setting setting = new Setting();
+		setting.setTeamId("TEAM-ID-123");
+		setting.setIdentifier("setting_123");
+		setting.setKey("hiv_prevalence");
+		setting.setValue("true");
+		setting.setDescription("The area HIV prevalence is more than 5%");
+		settingList.add(setting);
+		
+		List<SettingConfiguration> settingConfig = new ArrayList<>();
+		SettingConfiguration config = new SettingConfiguration();
+		config.setTeamId("TEAM-ID-123");
+		config.setIdentifier("setting_123");
+		config.setSettings(settingList);
+		settingConfig.add(config);
+		
+		when(settingService.findSettings(ArgumentMatchers.any(SettingSearchBean.class))).thenReturn(settingConfig);
+		
+		MvcResult result =
+				mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/{identifier}", "setting_123")).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+		
+		Mockito.verify(settingService).findSettings(ArgumentMatchers.any(SettingSearchBean.class));
+		Mockito.verifyNoMoreInteractions(settingService);
+		String responseString = result.getResponse().getContentAsString();
+		if (responseString.isEmpty()) {
+			AssertionErrors.fail("Test case failed");
+		}
+		JsonNode actualObj = mapper.readTree(responseString);
+		
+		Assert.assertEquals(actualObj.get(0).get("identifier").asText(), "setting_123");
+		Assert.assertEquals(actualObj.get(0).get("teamId").asText(), EXPECTED_TEAM_ID);
+		Assert.assertEquals(actualObj.get(0).get("key").asText(), "hiv_prevalence");
+		Assert.assertEquals(actualObj.get(0).get("value").asText(), "true");
+		Assert.assertEquals(1, actualObj.size());
+	}
+	
+	@Test
+	public void testGetAllSettings() throws Exception {
+		List<SettingConfiguration> settingConfig = new ArrayList<>();
+		SettingConfiguration config = new SettingConfiguration();
+		config.setTeamId("TEAM-ID-123");
+		config.setIdentifier("setting_123");
+		config.setSettings(createSettingsList());
+		settingConfig.add(config);
+		
+		when(settingService.findSettings(ArgumentMatchers.any(SettingSearchBean.class))).thenReturn(settingConfig);
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders.get(BASE_URL + "/").param(AllConstants.Event.TEAM_ID, EXPECTED_TEAM_ID).param(AllConstants.BaseEntity.SERVER_VERSIOIN, "15421904649873"))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+		Mockito.verify(settingService).findSettings(ArgumentMatchers.any(SettingSearchBean.class));
+		Mockito.verifyNoMoreInteractions(settingService);
+		JSONArray response = new JSONArray(result.getResponse().getContentAsString());
+		Assert.assertEquals(2, response.length());
+		Assert.assertEquals("setting_987", ((JSONObject) response.get(1)).get("identifier"));
+		Assert.assertEquals("setting_123", ((JSONObject) response.get(0)).get("identifier"));
+	}
+	
+	@Test
+	public void testGetAllSettingsWithoutAnyParams() throws Exception {
+		List<SettingConfiguration> settingConfig = new ArrayList<>();
+		SettingConfiguration config = new SettingConfiguration();
+		config.setTeamId("TEAM-ID-123");
+		config.setIdentifier("setting_123");
+		config.setSettings(createSettingsList());
+		settingConfig.add(config);
+		
+		Mockito.when(settingService.findSettings(ArgumentMatchers.any(SettingSearchBean.class))).thenReturn(settingConfig);
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders.get(BASE_URL + "/")).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+		Mockito.verifyNoInteractions(settingService);
+		JSONObject response = new JSONObject(result.getResponse().getContentAsString());
+		Assert.assertEquals(0, response.length());
+	}
+	
+	@Test
+	public void testCreateOrUpdate() throws Exception {
+		ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+		String EXPECTED_SETTINGS = "{\"type\":\"Setting\",\"identifier\":\"setting_123\",\"teamId\":\"TEAM-ID-123\",\"key\":\"hiv_prevalence\",\"value\":\"true\",\"description\":\"The area HIV prevalence is more than 5%\"}";
+		MvcResult result =
+				mockMvc.perform(MockMvcRequestBuilders.post(BASE_URL + "/").contentType(MediaType.APPLICATION_JSON).content(EXPECTED_SETTINGS.getBytes())).andExpect(MockMvcResultMatchers.status().isCreated()).andReturn();
+		Mockito.verify(settingService, Mockito.times(1)).saveSetting(argumentCaptor.capture());
+		Mockito.verifyNoMoreInteractions(settingService);
+		String settingConfiguration = argumentCaptor.getValue();
+		String EXPECTED_IDENTFIER = "setting_123";
+		Assert.assertTrue(settingConfiguration.contains(EXPECTED_IDENTFIER));
+	}
+	
+	@Test
+	public void testCreateOrUpdateWithWrongJson() throws Exception {
+		Mockito.when(settingService.saveSetting(ArgumentMatchers.any(String.class))).thenReturn("setting_123");
+		MvcResult result =
+				mockMvc.perform(MockMvcRequestBuilders.post(BASE_URL + "/").contentType(MediaType.APPLICATION_JSON).content(
+						"EXPECTED_SETTINGS.getBytes()")).andExpect(MockMvcResultMatchers.status().isBadRequest()).andReturn();
+		Mockito.verifyNoInteractions(settingService);
+		Assert.assertEquals("{}", result.getResponse().getContentAsString());
+	}
+	
+	private List<Setting> createSettingsList() {
+		List<Setting> settingList = new ArrayList<>();
+		Setting setting = new Setting();
+		setting.setTeamId("TEAM-ID-123");
+		setting.setIdentifier("setting_123");
+		setting.setKey("hiv_prevalence");
+		setting.setValue("true");
+		setting.setDescription("The area HIV prevalence is more than 5%");
+		
+		Setting setting1 = new Setting();
+		setting1.setTeamId("TEAM-ID-123");
+		setting1.setIdentifier("setting_987");
+		setting1.setKey("malaria_prevalence");
+		setting1.setValue("false");
+		setting1.setDescription("Malaria prevalence is more  5%");
+		
+		settingList.add(setting);
+		settingList.add(setting1);
+		return settingList;
+	}
+}
