b854907efa9f81cb81c2425e823b5be92013cc10
rehammuzzamil <62061579+rehammuzzamil@users.noreply.github.com>
2020-05-19 12:43:22 +0500

    309 : Create Identifier Sources API

--- src/main/java/org/opensrp/web/rest/IdentifierSourceResource.java
+++ src/main/java/org/opensrp/web/rest/IdentifierSourceResource.java
@@ -11,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/rest/identifiersource")
@@ -22,32 +19,22 @@ public class IdentifierSourceResource {
 
 	private static Logger logger = LoggerFactory.getLogger(IdentifierSourceResource.class.toString());
 
+	@Autowired
 	private IdentifierSourceService identifierSourceService;
 
+	@Autowired
 	protected ObjectMapper objectMapper;
 
-	public static final String IDENTIFIER = "identifier";
-
-	@Autowired
-	public void setObjectMapper(ObjectMapper objectMapper) {
-		this.objectMapper = objectMapper;
-	}
-
-	@Autowired
-	public IdentifierSourceResource(IdentifierSourceService identifierSourceService) {
-		this.identifierSourceService = identifierSourceService;
-	}
-
-	@RequestMapping(value = "getAll", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<String> get() throws JsonProcessingException {
+	@GetMapping(value = "/getAll", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<String> getAll() throws JsonProcessingException {
 		return new ResponseEntity<>(objectMapper.writeValueAsString(
 				identifierSourceService.findAllIdentifierSources()),
 				RestUtils.getJSONUTF8Headers(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/{identifier}", method = RequestMethod.GET, produces = {
+	@GetMapping(value = "/{identifier}", produces = {
 			MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<String> getIdentifierSourceByIdentifier(@PathVariable(IDENTIFIER) String identifier)
+	public ResponseEntity<String> getByIdentifier(@PathVariable String identifier)
 			throws JsonProcessingException {
 
 		return new ResponseEntity<>(objectMapper.writeValueAsString(
@@ -56,7 +43,7 @@ public class IdentifierSourceResource {
 				HttpStatus.OK);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE,
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE,
 			MediaType.TEXT_PLAIN_VALUE })
 	public ResponseEntity<String> createOrUpdate(@RequestBody IdentifierSource identifierSource) {
 		try {
