cd364e48517b5e527d90203a646d92bc2a015450
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-05-26 05:02:27 +0300

    :zap: update the core version & the settings resource to ensure v1 endpoint calls are formatted differently

--- src/main/java/org/opensrp/web/rest/SettingResource.java
+++ src/main/java/org/opensrp/web/rest/SettingResource.java
@@ -2,6 +2,7 @@ package org.opensrp.web.rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import org.apache.http.util.TextUtils;
 import org.joda.time.DateTime;
 import org.json.JSONArray;
@@ -26,8 +27,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static java.text.MessageFormat.format;
 import static org.opensrp.common.AllConstants.Event.LOCATION_ID;
@@ -37,7 +39,7 @@ import static org.opensrp.web.rest.RestUtils.getStringFilter;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
-@RequestMapping(value = "/rest/settings")
+@RequestMapping (value = "/rest/settings")
 public class SettingResource {
 	
 	private SettingService settingService;
@@ -47,15 +49,16 @@ public class SettingResource {
 	private final String TEAM_ID = "teamId";
 	
 	private final Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-	        .registerTypeAdapter(DateTime.class, new DateTimeTypeConverter()).create();
+			.registerTypeAdapter(DateTime.class, new DateTimeTypeConverter()).create();
 	
 	@Autowired
 	public void setSettingService(SettingService settingService) {
 		this.settingService = settingService;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/sync")
-	public @ResponseBody ResponseEntity<String> findSettingsByVersion(HttpServletRequest request) {
+	@RequestMapping (method = RequestMethod.GET, value = "/sync")
+	public @ResponseBody
+	ResponseEntity<String> findSettingsByVersion(HttpServletRequest request) {
 		JSONObject response = new JSONObject();
 		ResponseEntity responseEntity;
 		
@@ -67,56 +70,57 @@ public class SettingResource {
 			String locationId = getStringFilter(LOCATION_ID, request);
 			String team = getStringFilter(TEAM, request);
 			String teamId = getStringFilter(TEAM_ID, request);
-
+			
 			if (TextUtils.isBlank(serverVersion)) {
 				return new ResponseEntity<>(response.toString(), responseHeaders, HttpStatus.BAD_REQUEST);
 			}
-
+			
 			SettingSearchBean settingQueryBean = new SettingSearchBean();
 			settingQueryBean.setTeam(team);
 			settingQueryBean.setTeamId(teamId);
 			settingQueryBean.setProviderId(providerId);
 			settingQueryBean.setLocationId(locationId);
 			settingQueryBean.setServerVersion(Long.valueOf(serverVersion) + 1);
-
+			settingQueryBean.setV1Settings(true);
+			
 			List<SettingConfiguration> SettingConfigurations = settingService.findSettings(settingQueryBean);
-
+			
 			SettingTypeHandler settingTypeHandler = new SettingTypeHandler();
-			String settingsArrayString = BaseTypeHandler.mapper.writeValueAsString(SettingConfigurations);
-
+			String settingsArrayString = settingTypeHandler.mapper.writeValueAsString(SettingConfigurations);
+			
 			responseEntity = new ResponseEntity<>(new JSONArray(settingsArrayString).toString(), responseHeaders, HttpStatus.OK); // todo: why is this conversion to json array necessary?
 		} catch (Exception e) {
 			logger.error(format("Sync data processing failed with exception {0}.- ", e));
 			responseEntity = new ResponseEntity<>(responseHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-
+		
 		return responseEntity;
 	}
 	
-	@RequestMapping(headers = { "Accept=application/json" }, method = POST, value = "/sync")
+	@RequestMapping (headers = {"Accept=application/json"}, method = POST, value = "/sync")
 	public ResponseEntity<String> saveSetting(@RequestBody String data) {
 		JSONObject response = new JSONObject();
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.add("Content-Type", "application/json; charset=utf-8");
-
+		
 		JSONObject syncData = new JSONObject(data);
-
+		
 		if (!syncData.has("settingConfigurations")) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		} else {
-
+			
 			JSONArray clientSettings = syncData.getJSONArray(AllConstants.Event.SETTING_CONFIGURATIONS);
 			JSONArray dbSettingsArray = new JSONArray();
-
+			
 			for (int i = 0; i < clientSettings.length(); i++) {
 				String settingConfigurationsString = clientSettings.getJSONObject(i).toString();
 				dbSettingsArray.put(settingService.saveSetting(settingConfigurationsString));
 			}
-
+			
 			response.put("validated_records", dbSettingsArray);
-
+			
 		}
-
+		
 		return new ResponseEntity<>(response.toString(), responseHeaders, HttpStatus.OK);
 	}
 }
