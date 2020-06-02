6462011205ee26b9f2341121d064f9420755a54f
Samuel Githengi <samuel.githengi@gmail.com>
2020-06-02 19:15:30 +0300

    Code cleanup

--- src/main/java/org/opensrp/web/config/security/HttpSessionConfig.java
+++ src/main/java/org/opensrp/web/config/security/HttpSessionConfig.java
@@ -1,13 +1,9 @@
 package org.opensrp.web.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
-import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
 @EnableRedisHttpSession
@@ -16,13 +12,4 @@ public class HttpSessionConfig {
 	@Autowired
 	private RedisConnectionFactory redisConnectionFactory;
 
-	/*@Bean
-	public HttpSessionIdResolver httpSessionIdResolver() {
-		return HeaderHttpSessionIdResolver.xAuthToken();
-	}
-	
-	@Bean
-	public static ConfigureRedisAction configureRedisAction() {
-		return ConfigureRedisAction.NO_OP;
-	}*/
 }
