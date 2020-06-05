4434e45e8d791d2d0f1df673e765a3209083134c
Ephraim Kigamba <ekigamba@ona.io>
2020-06-05 17:22:51 +0300

    Add ClientFormValidatorTest file

---
+++ src/test/java/org/opensrp/web/utils/ClientFormValidatorTest.java
@@ -0,0 +1,27 @@
+package org.opensrp.web.utils;
+
+import junit.framework.TestCase;
+
+public class ClientFormValidatorTest extends TestCase {
+
+    public void testCheckForMissingFormReferencesShouldReturnEmptyHashSetWhenGiveSubForm() {
+    }
+
+    public void testCheckForMissingFormReferencesShouldReturnEmptyHashSetWhenGivenFormWithoutSubFormReferences() {
+    }
+
+    public void testCheckForMissingFormReferencesShouldReturnNonEmptyHashSetWhenGivenFormWithoutMissingSubFormReferences() {
+    }
+
+    public void testCheckForMissingFormReferencesShouldReturnEmptyHashSetWhenGivenFormWithAvailableSubFormReferences() {
+    }
+
+    public void testCheckForMissingRuleReferencesShouldReturnEmptyHashSetWhenGivenFormWithoutRuleFileReferences() {
+    }
+
+    public void testCheckForMissingRuleReferencesShouldReturnNonEmptyHashSetWhenGivenFormWithMissingRuleFileReferences() {
+    }
+
+    public void testCheckForMissingRuleReferencesShouldReturnEmptyHashSetWhenGivenFormWithAvailbleRuleFiles() {
+    }
+}
