83a271f07680a8148584b0fcf93806886f62e6bc
rkodev <43806892+rkodev@users.noreply.github.com>
2020-06-11 12:55:50 +0300

    Bussinesslogicexception fix

--- src/main/java/org/opensrp/web/exceptions/BusinessLogicException.java
+++ src/main/java/org/opensrp/web/exceptions/BusinessLogicException.java
@@ -3,16 +3,6 @@ package org.opensrp.web.exceptions;
 public class BusinessLogicException extends Exception {
 
 	public BusinessLogicException(String error) {
-		this.businessLogicError = error;
-	}
-
-	private String businessLogicError;
-
-	public String getBusinessLogicError() {
-		return businessLogicError;
-	}
-
-	public void setBusinessLogicError(String businessLogicError) {
-		this.businessLogicError = businessLogicError;
+		super(error);
 	}
 }
