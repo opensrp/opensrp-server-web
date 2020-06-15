d87c499b5bbb75cf24a20f87722df0384c5ffcea
Allan O <a.onchuru@gmail.com>
2020-06-15 17:54:41 +0300

    :construction: Add (optional) form relation param

--- src/main/java/org/opensrp/web/rest/ClientFormResource.java
+++ src/main/java/org/opensrp/web/rest/ClientFormResource.java
@@ -144,6 +144,7 @@ public class ClientFormResource {
     @RequestMapping(headers = {"Accept=multipart/form-data"}, method = RequestMethod.POST)
     private ResponseEntity<String> addClientForm(@RequestParam("form_version") String formVersion,
                                                  @RequestParam(value = "form_identifier", required = false) String formIdentifier,
+                                                 @RequestParam(value = "form_relation", required = false) String relation,
                                                  @RequestParam("form_name") String formName,
                                                  @RequestParam("form") MultipartFile jsonFile,
                                                  @RequestParam(required = false) String module) {
@@ -203,7 +204,7 @@ public class ClientFormResource {
         }
 
         String identifier = formIdentifier;
-        if (TextUtils.isEmpty(formIdentifier)){
+        if (TextUtils.isBlank(formIdentifier)){
             identifier = Paths.get(jsonFile.getOriginalFilename()).getFileName().toString();
         }
 
@@ -217,6 +218,10 @@ public class ClientFormResource {
         clientFormMetadata.setLabel(formName);
         clientFormMetadata.setCreatedAt(new Date());
         clientFormMetadata.setModule(module);
+
+        if (!TextUtils.isBlank(relation)){
+            clientFormMetadata.setRelation(relation);
+        }
 
         ClientFormService.CompleteClientForm completeClientForm = clientFormService.addClientForm(clientForm, clientFormMetadata);
 
