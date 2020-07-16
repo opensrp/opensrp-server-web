bbf0704f96acee9e4409ecc75f4fb6a4f8b31e0f
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-07-16 14:07:54 +0300

    :construction: use lombok & enable a no arguments constructor

--- src/main/java/org/opensrp/web/bean/JsonWidgetValidatorDefinition.java
+++ src/main/java/org/opensrp/web/bean/JsonWidgetValidatorDefinition.java
@@ -4,47 +4,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class JsonWidgetValidatorDefinition {
 
 	@JsonProperty(value = "cannot_remove")
 	private WidgetCannotRemove cannotRemove;
 
-	public JsonWidgetValidatorDefinition() {
-		super();
-	}
-
-	public JsonWidgetValidatorDefinition(WidgetCannotRemove cannotRemove) {
-		this.cannotRemove = cannotRemove;
-	}
-
-	public WidgetCannotRemove getCannotRemove() {
-		return cannotRemove;
-	}
-
-	public void setCannotRemove(WidgetCannotRemove cannotRemove) {
-		this.cannotRemove = cannotRemove;
-	}
-
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@ToString
 	public static class WidgetCannotRemove {
 
 		private String title;
 
 		private ArrayList<String> fields;
-
-		public String getTitle() {
-			return title;
-		}
-
-		public void setTitle(String title) {
-			this.title = title;
-		}
-
-		public ArrayList<String> getFields() {
-			return fields;
-		}
-
-		public void setFields(ArrayList<String> fields) {
-			this.fields = fields;
-		}
 	}
 }
