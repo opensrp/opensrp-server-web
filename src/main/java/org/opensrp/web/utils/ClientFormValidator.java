cb3d04e70d3a78b66a41004511cb6efd93bb16fd
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-07-16 07:08:39 +0300

    :construction: add deserialization configs

--- src/main/java/org/opensrp/web/utils/ClientFormValidator.java
+++ src/main/java/org/opensrp/web/utils/ClientFormValidator.java
@@ -1,12 +1,14 @@
 package org.opensrp.web.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
+
 import org.opensrp.domain.postgres.ClientForm;
 import org.opensrp.service.ClientFormService;
 import org.opensrp.web.Constants;
@@ -17,165 +19,186 @@ import org.springframework.lang.NonNull;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.composer.ComposerException;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class ClientFormValidator {
-    private static final Logger logger = LoggerFactory.getLogger(ClientFormValidator.class.toString());
 
-    private ArrayList<String> jsonPathForSubFormReferences = new ArrayList<>();
-    private ArrayList<String> jsonPathForRuleReferences = new ArrayList<>();
-    private ArrayList<String> jsonPathForPropertyFileReferences = new ArrayList<>();
-    private ClientFormService clientFormService;
-    private static String PROPERTIES_FILE_NAME = "properties_file_name";
+	private static final Logger logger = LoggerFactory.getLogger(ClientFormValidator.class.toString());
 
-    public ClientFormValidator(@NonNull ClientFormService clientFormService) {
-        this.clientFormService = clientFormService;
+	private final ArrayList<String> jsonPathForSubFormReferences = new ArrayList<>();
 
-        initialiseSubFormJsonPathReferences();
-        initialiseRuleJsonPathReferences();
-        initialisePropertiesFileJsonPathReferences();
-    }
+	private final ArrayList<String> jsonPathForRuleReferences = new ArrayList<>();
 
-    private void initialiseSubFormJsonPathReferences() {
-        jsonPathForSubFormReferences.add("$.*.fields[*][?(@.type == 'native_radio')].options[*].content_form");
-        jsonPathForSubFormReferences.add("$.*.fields[*][?(@.type == 'expansion_panel')].content_form");
-    }
+	private final ArrayList<String> jsonPathForPropertyFileReferences = new ArrayList<>();
 
-    private void initialiseRuleJsonPathReferences() {
-        jsonPathForRuleReferences.add("$.*.fields[*].calculation.rules-engine.ex-rules.rules-file");
-        jsonPathForRuleReferences.add("$.*.fields[*].relevance.rules-engine.ex-rules.rules-file");
-        jsonPathForRuleReferences.add("$.*.fields[*].constraints.rules-engine.ex-rules.rules-file");
-    }
+	private final ClientFormService clientFormService;
 
-    private void initialisePropertiesFileJsonPathReferences() {
-        jsonPathForPropertyFileReferences.add("$.properties_file_name");
-    }
+	private static final String PROPERTIES_FILE_NAME = "properties_file_name";
 
-    @NonNull
-    public HashSet<String> checkForMissingFormReferences(@NonNull String jsonForm) {
-        HashSet<String> subFormReferences = new HashSet<>();
-        HashSet<String> missingSubFormReferences = new HashSet<>();
+	public ClientFormValidator(@NonNull ClientFormService clientFormService) {
+		this.clientFormService = clientFormService;
 
-        for (String jsonPath: jsonPathForSubFormReferences) {
-            List<String> references = JsonPath.read(jsonForm, jsonPath);
-            subFormReferences.addAll(references);
-        }
+		initialiseSubFormJsonPathReferences();
+		initialiseRuleJsonPathReferences();
+		initialisePropertiesFileJsonPathReferences();
+	}
 
-        // Check if the references exist in the DB
-        for (String subFormReference: subFormReferences) {
-            // If the form does not exist, Add a .json extension & check again
-            if (!clientFormService.isClientFormExists(subFormReference) && !clientFormService.isClientFormExists(subFormReference + ".json")) {
-                missingSubFormReferences.add(subFormReference);
-            }
-        }
+	private void initialiseSubFormJsonPathReferences() {
+		jsonPathForSubFormReferences.add("$.*.fields[*][?(@.type == 'native_radio')].options[*].content_form");
+		jsonPathForSubFormReferences.add("$.*.fields[*][?(@.type == 'expansion_panel')].content_form");
+	}
 
-        return missingSubFormReferences;
-    }
+	private void initialiseRuleJsonPathReferences() {
+		jsonPathForRuleReferences.add("$.*.fields[*].calculation.rules-engine.ex-rules.rules-file");
+		jsonPathForRuleReferences.add("$.*.fields[*].relevance.rules-engine.ex-rules.rules-file");
+		jsonPathForRuleReferences.add("$.*.fields[*].constraints.rules-engine.ex-rules.rules-file");
+	}
 
-    @NonNull
-    public HashSet<String> checkForMissingRuleReferences(@NonNull String jsonForm) {
-        HashSet<String> ruleFileReferences = new HashSet<>();
-        HashSet<String> missingRuleFileReferences = new HashSet<>();
+	private void initialisePropertiesFileJsonPathReferences() {
+		jsonPathForPropertyFileReferences.add("$.properties_file_name");
+	}
 
-        for (String jsonPath: jsonPathForRuleReferences) {
-            List<String> references = JsonPath.read(jsonForm, jsonPath);
-            ruleFileReferences.addAll(references);
-        }
+	@NonNull
+	public HashSet<String> checkForMissingFormReferences(@NonNull String jsonForm) {
+		HashSet<String> subFormReferences = new HashSet<>();
+		HashSet<String> missingSubFormReferences = new HashSet<>();
 
-        // Check if the references exist in the DB
-        for (String ruleFileReference: ruleFileReferences) {
-            if (!clientFormService.isClientFormExists(ruleFileReference)) {
-                missingRuleFileReferences.add(ruleFileReference);
-            }
-        }
+		for (String jsonPath : jsonPathForSubFormReferences) {
+			List<String> references = JsonPath.read(jsonForm, jsonPath);
+			subFormReferences.addAll(references);
+		}
 
-        return missingRuleFileReferences;
-    }
+		// Check if the references exist in the DB
+		for (String subFormReference : subFormReferences) {
+			// If the form does not exist, Add a .json extension & check again
+			if (!clientFormService.isClientFormExists(subFormReference) && !clientFormService
+					.isClientFormExists(subFormReference + ".json")) {
+				missingSubFormReferences.add(subFormReference);
+			}
+		}
 
-    @NonNull
-    public HashSet<String> checkForMissingPropertyFileReferences(@NonNull String jsonForm) {
-        Configuration conf = Configuration.defaultConfiguration()
-                .addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL);
+		return missingSubFormReferences;
+	}
 
-        HashSet<String> propertyFileReferences = new HashSet<>();
-        HashSet<String> missingPropertyFileReferences = new HashSet<>();
+	@NonNull
+	public HashSet<String> checkForMissingRuleReferences(@NonNull String jsonForm) {
+		HashSet<String> ruleFileReferences = new HashSet<>();
+		HashSet<String> missingRuleFileReferences = new HashSet<>();
 
-        for (String jsonPath: jsonPathForPropertyFileReferences) {
-            String reference = (JsonPath.using(conf)).parse(jsonForm).read(jsonPath);
+		for (String jsonPath : jsonPathForRuleReferences) {
+			List<String> references = JsonPath.read(jsonForm, jsonPath);
+			ruleFileReferences.addAll(references);
+		}
 
-            if (reference != null) {
-                propertyFileReferences.add(reference);
-            }
-        }
+		// Check if the references exist in the DB
+		for (String ruleFileReference : ruleFileReferences) {
+			if (!clientFormService.isClientFormExists(ruleFileReference)) {
+				missingRuleFileReferences.add(ruleFileReference);
+			}
+		}
 
-        // Check if the references exist in the DB
-        for (String propertyFileReference: propertyFileReferences) {
-            if (!clientFormService.isClientFormExists(propertyFileReference) && !clientFormService.isClientFormExists(propertyFileReference + ".properties")) {
-                missingPropertyFileReferences.add(propertyFileReference);
-            }
-        }
+		return missingRuleFileReferences;
+	}
 
-        return missingPropertyFileReferences;
-    }
+	@NonNull
+	public HashSet<String> checkForMissingPropertyFileReferences(@NonNull String jsonForm) {
+		Configuration conf = Configuration.defaultConfiguration()
+				.addOptions(Option.DEFAULT_PATH_LEAF_TO_NULL);
 
-    public HashSet<String> checkForMissingYamlPropertyFileReferences(@NonNull String fileContent) {
-        HashSet<String> propertyFileReferences = new HashSet<>();
-        HashSet<String> missingPropertyFileReferences = new HashSet<>();
-        try {
-            Map<Object, Object> document = new Yaml().load(fileContent);
-            if (document.containsKey(PROPERTIES_FILE_NAME)) {
-                propertyFileReferences.add((String) document.get(PROPERTIES_FILE_NAME));
-            }
-            // Check if the references exist in the DB
-            for (String propertyFileReference: propertyFileReferences) {
-                if (!clientFormService.isClientFormExists(propertyFileReference) && !clientFormService.isClientFormExists(propertyFileReference + ".properties")) {
-                    missingPropertyFileReferences.add(propertyFileReference);
-                }
-            }
-        }catch (ComposerException exception) {
-            logger.error("Validator parsing a YAML file that doesn't conform in structure", exception);
-            return missingPropertyFileReferences;
-        }
-        return missingPropertyFileReferences;
-    }
+		HashSet<String> propertyFileReferences = new HashSet<>();
+		HashSet<String> missingPropertyFileReferences = new HashSet<>();
 
-    @NonNull
-    public HashSet<String> performWidgetValidation(@NonNull ObjectMapper objectMapper, @NonNull String formIdentifier, @NonNull String clientFormContent) throws JsonProcessingException {
-        ClientForm formValidator = clientFormService.getMostRecentFormValidator(formIdentifier);
-        HashSet<String> fieldsMap = new HashSet<>();
-        if (formValidator != null && formValidator.getJson() != null && !formValidator.getJson().toString().isEmpty()) {
-            JsonWidgetValidatorDefinition jsonWidgetValidatorDefinition =
-                    objectMapper.readValue(formValidator.getJson().toString(), JsonWidgetValidatorDefinition.class);
-            JsonWidgetValidatorDefinition.WidgetCannotRemove widgetCannotRemove = jsonWidgetValidatorDefinition.getCannotRemove();
-            if (widgetCannotRemove != null && widgetCannotRemove.getFields() != null && widgetCannotRemove.getFields().size() > 0) {
-                JsonNode jsonNode = objectMapper.readTree(clientFormContent);
-                fieldsMap.addAll(widgetCannotRemove.getFields());
+		for (String jsonPath : jsonPathForPropertyFileReferences) {
+			String reference = (JsonPath.using(conf)).parse(jsonForm).read(jsonPath);
 
-                Iterator<Map.Entry<String, JsonNode>> iterator = jsonNode.fields();
-                while (iterator.hasNext()) {
-                    Map.Entry<String, JsonNode> jsonFormField = iterator.next();
-                    if (jsonFormField.getKey().startsWith(Constants.JsonForm.Key.STEP)) {
-                        JsonNode step = jsonFormField.getValue();
-                        if (step.has(Constants.JsonForm.Key.FIELDS)) {
-                            JsonNode fields = step.get(Constants.JsonForm.Key.FIELDS);
-                            if (fields instanceof ArrayNode) {
-                                ArrayNode fieldsArray = (ArrayNode) fields;
-                                for (int i = 0; i < fieldsArray.size(); i++) {
-                                    JsonNode jsonNodeField = fieldsArray.get(i);
-                                    String key = jsonNodeField.get(Constants.JsonForm.Key.KEY).asText();
+			if (reference != null) {
+				propertyFileReferences.add(reference);
+			}
+		}
 
-                                    if (fieldsMap.remove(key) && fieldsMap.size() == 0) {
-                                        return fieldsMap;
-                                    }
-                                }
-                            }
-                        }
-                    }
+		// Check if the references exist in the DB
+		for (String propertyFileReference : propertyFileReferences) {
+			if (!clientFormService.isClientFormExists(propertyFileReference) && !clientFormService
+					.isClientFormExists(propertyFileReference + ".properties")) {
+				missingPropertyFileReferences.add(propertyFileReference);
+			}
+		}
 
-                }
-            }
-        }
+		return missingPropertyFileReferences;
+	}
 
-        return fieldsMap;
-    }
+	public HashSet<String> checkForMissingYamlPropertyFileReferences(@NonNull String fileContent) {
+		HashSet<String> propertyFileReferences = new HashSet<>();
+		HashSet<String> missingPropertyFileReferences = new HashSet<>();
+		try {
+			Map<Object, Object> document = new Yaml().load(fileContent);
+			if (document.containsKey(PROPERTIES_FILE_NAME)) {
+				propertyFileReferences.add((String) document.get(PROPERTIES_FILE_NAME));
+			}
+			// Check if the references exist in the DB
+			for (String propertyFileReference : propertyFileReferences) {
+				if (!clientFormService.isClientFormExists(propertyFileReference) && !clientFormService
+						.isClientFormExists(propertyFileReference + ".properties")) {
+					missingPropertyFileReferences.add(propertyFileReference);
+				}
+			}
+		}
+		catch (ComposerException exception) {
+			logger.error("Validator parsing a YAML file that doesn't conform in structure", exception);
+			return missingPropertyFileReferences;
+		}
+		return missingPropertyFileReferences;
+	}
+
+	@NonNull
+	public HashSet<String> performWidgetValidation(@NonNull ObjectMapper objectMapper, @NonNull String formIdentifier,
+			@NonNull String clientFormContent) throws JsonProcessingException {
+		ClientForm formValidator = clientFormService.getMostRecentFormValidator(formIdentifier);
+		HashSet<String> fieldsMap = new HashSet<>();
+
+		if (formValidator != null && formValidator.getJson() != null && !((String) formValidator.getJson()).isEmpty()) {
+			logger.info((String) formValidator.getJson());
+			objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT);
+			objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+
+			JsonWidgetValidatorDefinition jsonWidgetValidatorDefinition =
+					objectMapper.readValue(((String) formValidator.getJson()), JsonWidgetValidatorDefinition.class);
+			JsonWidgetValidatorDefinition.WidgetCannotRemove widgetCannotRemove = jsonWidgetValidatorDefinition
+					.getCannotRemove();
+
+			if (widgetCannotRemove != null && widgetCannotRemove.getFields() != null
+					&& widgetCannotRemove.getFields().size() > 0) {
+				JsonNode jsonNode = objectMapper.readTree(clientFormContent);
+				fieldsMap.addAll(widgetCannotRemove.getFields());
+
+				Iterator<Map.Entry<String, JsonNode>> iterator = jsonNode.fields();
+				while (iterator.hasNext()) {
+					Map.Entry<String, JsonNode> jsonFormField = iterator.next();
+					if (jsonFormField.getKey().startsWith(Constants.JsonForm.Key.STEP)) {
+						JsonNode step = jsonFormField.getValue();
+						if (step.has(Constants.JsonForm.Key.FIELDS)) {
+							JsonNode fields = step.get(Constants.JsonForm.Key.FIELDS);
+							if (fields instanceof ArrayNode) {
+								ArrayNode fieldsArray = (ArrayNode) fields;
+								for (int i = 0; i < fieldsArray.size(); i++) {
+									JsonNode jsonNodeField = fieldsArray.get(i);
+									String key = jsonNodeField.get(Constants.JsonForm.Key.KEY).asText();
+
+									if (fieldsMap.remove(key) && fieldsMap.size() == 0) {
+										return fieldsMap;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return fieldsMap;
+	}
 }
