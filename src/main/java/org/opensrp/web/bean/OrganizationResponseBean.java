5dd240345a68ef5642881c06fb42e19fe8e2a0c9
proshanto <proshanto123@gmail.com>
2020-07-17 16:34:44 +0600

    remove unnecessary object from OrganizationResponseBean class

--- src/main/java/org/opensrp/web/bean/OrganizationResponseBean.java
+++ src/main/java/org/opensrp/web/bean/OrganizationResponseBean.java
@@ -4,16 +4,9 @@ import java.util.List;
 
 import org.opensrp.domain.Organization;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class OrganizationResponseBean {
 	
 	private List<Organization> organizations;
-	
-	@JsonProperty("total")
-	private Integer total;
-	
-	private String msg;
 	
 	public List<Organization> getOrganizations() {
 		return organizations;
@@ -21,22 +14,6 @@ public class OrganizationResponseBean {
 	
 	public void setOrganizations(List<Organization> organizations) {
 		this.organizations = organizations;
-	}
-	
-	public Integer getTotal() {
-		return total;
-	}
-	
-	public void setTotal(Integer total) {
-		this.total = total;
-	}
-	
-	public String getMsg() {
-		return msg;
-	}
-	
-	public void setMsg(String msg) {
-		this.msg = msg;
 	}
 	
 }
