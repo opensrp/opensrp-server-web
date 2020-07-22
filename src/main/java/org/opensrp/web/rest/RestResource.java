1f5d803dfecfdcc1fc72178f29da472e4db97131
Samuel Githengi <samuel.githengi@gmail.com>
2020-07-22 10:14:31 +0300

    Base rest resource privide base type

--- src/main/java/org/opensrp/web/rest/RestResource.java
+++ src/main/java/org/opensrp/web/rest/RestResource.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.smartregister.domain.BaseDataEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public abstract class RestResource<T> {
+public abstract class RestResource<T  extends BaseDataEntity> {
 	
 	protected ObjectMapper objectMapper;
 	
