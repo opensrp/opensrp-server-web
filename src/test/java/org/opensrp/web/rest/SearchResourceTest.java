c2bd4ac087e09acd1a50f6d577d97b45c10be04e
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2020-07-22 11:26:29 +0300

    :ok_hand: Remove unused imports

--- src/test/java/org/opensrp/web/rest/SearchResourceTest.java
+++ src/test/java/org/opensrp/web/rest/SearchResourceTest.java
@@ -1,21 +1,11 @@
 
 package org.opensrp.web.rest;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.opensrp.common.AllConstants;
-import org.opensrp.domain.setting.SettingConfiguration;
-import org.opensrp.search.SettingSearchBean;
-import org.smartregister.domain.Client;
 import org.opensrp.repository.ClientsRepository;
 import org.opensrp.repository.EventsRepository;
 import org.opensrp.repository.SearchRepository;
@@ -24,20 +14,14 @@ import org.opensrp.service.EventService;
 import org.opensrp.service.SearchService;
 import org.opensrp.web.rest.it.TestWebContextLoader;
 import org.opensrp.web.utils.SearchHelper;
+import org.smartregister.domain.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.springframework.test.web.AssertionErrors.fail;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = TestWebContextLoader.class, locations = { "classpath:test-webmvc-config.xml", })
@@ -51,17 +35,13 @@ public class SearchResourceTest {
 	private ClientService clientService;
 	
 	private EventService eventService;
-	
-	private SearchRepository searchRepository;
-	
-	private ClientsRepository clientRepository;
-	
-	private EventsRepository eventsRepository;
-	
+
 	@Before
 	public void setUp() {
-		
-		searchRepository = Mockito.mock(SearchRepository.class);
+		SearchRepository searchRepository = Mockito.mock(SearchRepository.class);
+		ClientsRepository clientRepository = Mockito.mock(ClientsRepository.class);
+		EventsRepository eventsRepository = Mockito.mock(EventsRepository.class);
+
 		searchService = Mockito.spy(new SearchService(searchRepository));
 		clientService = Mockito.spy(new ClientService(clientRepository));
 		eventService = Mockito.spy(new EventService(eventsRepository, clientService));
