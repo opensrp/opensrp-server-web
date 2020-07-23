917a8082fcf27ee6a70aef00882734f5bbef063f
Samuel Githengi <samuel.githengi@gmail.com>
2020-07-23 19:19:39 +0300

    expose keycloak users via proxy API

--- src/main/java/org/opensrp/web/rest/UserResource.java
+++ src/main/java/org/opensrp/web/rest/UserResource.java
@@ -4,6 +4,8 @@
 package org.opensrp.web.rest;
 
 import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONObject;
 import org.keycloak.adapters.KeycloakDeployment;
@@ -16,9 +18,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpStatusCodeException;
 
@@ -40,6 +43,9 @@ public class UserResource {
 	@Value("#{opensrp['keycloak.password.reset.endpoint']}")
 	private String resetPasswordURL;
 	
+	@Value("#{opensrp['keycloak.users.endpoint']}")
+	private String usersURL;
+	
 	/**
 	 * @param userService the userService to set
 	 */
@@ -48,7 +54,7 @@ public class UserResource {
 		this.userService = userService;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	private ResponseEntity<String> getAllUsers(@RequestParam("page_size") int limit,
 	        @RequestParam("start_index") int offset) {
 		
@@ -57,7 +63,46 @@ public class UserResource {
 		
 	}
 	
-	@RequestMapping(value = "/reset-password", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/keycloak")
+	private ResponseEntity<String> getAllKeycloakUsers(@RequestParam("page_size") int limit,
+	        @RequestParam("start_index") int offset) {
+		
+		String url = MessageFormat.format(usersURL, keycloakDeployment.getAuthServerBaseUrl(),
+		    keycloakDeployment.getRealm());
+		
+		ResponseEntity<String> response = null;
+		Map<String, Integer> uriVariables = new HashMap<>();
+		uriVariables.put("first", offset);
+		uriVariables.put("max", limit);
+		try {
+			
+			response = restTemplate.getForEntity(url, String.class, uriVariables);
+		}
+		catch (HttpStatusCodeException e) {
+			return new ResponseEntity<String>(e.getResponseBodyAsString(), e.getStatusCode());
+		}
+		return response;
+		
+	}
+	
+	@GetMapping(value = "/keycloak/count")
+	private ResponseEntity<String> getKeycloakUsersCount(@RequestParam("page_size") int limit,
+	        @RequestParam("start_index") int offset) {
+		
+		String url = MessageFormat.format(usersURL, keycloakDeployment.getAuthServerBaseUrl(), keycloakDeployment.getRealm())
+		        + "/count";
+		ResponseEntity<String> response = null;
+		try {	
+			response = restTemplate.getForEntity(url, String.class);
+		}
+		catch (HttpStatusCodeException e) {
+			return new ResponseEntity<String>(e.getResponseBodyAsString(), e.getStatusCode());
+		}
+		return response;
+		
+	}
+	
+	@PostMapping(value = "/reset-password", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	private ResponseEntity<String> changePassword(@RequestBody ResetPasswordBean resetPasswordBean) {
 		String url = MessageFormat.format(resetPasswordURL, keycloakDeployment.getAuthServerBaseUrl(),
 		    keycloakDeployment.getRealm());
