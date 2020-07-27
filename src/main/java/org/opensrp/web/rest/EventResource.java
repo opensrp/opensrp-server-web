8961034332243302fe2cf3850af682ea9c980198
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-07-27 19:49:07 +0500

    Passing Authentication.getName() in eventService.addEvent() method

--- src/main/java/org/opensrp/web/rest/EventResource.java
+++ src/main/java/org/opensrp/web/rest/EventResource.java
@@ -53,6 +53,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -400,7 +401,7 @@ public class EventResource extends RestResource<Event> {
 
 	@Override
 	public Event create(Event o) {
-		return eventService.addEvent(o, ""); //TODO?
+		return eventService.addEvent(o, SecurityContextHolder.getContext().getAuthentication().getName());
 	}
 
 	@Override
