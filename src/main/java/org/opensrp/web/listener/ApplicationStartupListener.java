f6bb6d13287522403d81ad703c589af1a3e8f98f
Martin Ndegwa <ndegwamartin@gmail.com>
2020-07-28 22:34:38 +0300

    Migrate opensrp server connector library - Refactor ApplicationStartupListener to fetch openmrs locations from helper class

--- src/main/java/org/opensrp/web/listener/ApplicationStartupListener.java
+++ src/main/java/org/opensrp/web/listener/ApplicationStartupListener.java
@@ -1,15 +1,9 @@
 package org.opensrp.web.listener;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import org.apache.commons.lang3.StringUtils;
-import org.json.JSONArray;
-import org.opensrp.connector.openmrs.service.OpenmrsLocationService;
+import org.opensrp.connector.openmrs.FetchLocationsHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -21,41 +15,19 @@ public class ApplicationStartupListener implements ApplicationListener<ContextRe
 	
 	public static final String APPLICATION_ID = "/opensrp";
 	
-	@Value("#{opensrp['openmrs.location.cache.uuids']}")
-	private String cacheLocationIds;
-	
-	@Value("#{opensrp['openmrs.location.cache.level'] ?: 'District'}")
-	private String cacheLocationLevel;
-	
-	@Value("#{opensrp['openmrs.location.cache.tags'] ?: 'MOH Jhpiego Facility Name,Health Facility,Facility'}")
-	private String cacheLocationTag;
-	
 	@Autowired
-	private OpenmrsLocationService openmrsLocationService;
+	private FetchLocationsHelper fetchLocationsHelper;
 	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 		logger.info(contextRefreshedEvent.getApplicationContext().getId());
-		if (contextRefreshedEvent.getApplicationContext().getId().endsWith(APPLICATION_ID + APPLICATION_ID)
-		        && StringUtils.isNotBlank(cacheLocationIds)) {
-			ExecutorService executor = Executors.newFixedThreadPool(5);
-			String[] locations = cacheLocationIds.split(",");
-			final String[] locationTags = cacheLocationTag.split(",");
-			for (final String id : locations) {
-				executor.submit(new Runnable() {
-					
-					@Override
-					public void run() {
-						JSONArray tags = new JSONArray(locationTags);
-						logger.info(String.format("populating OpenMRS location cache for %s, %s, %s ", id,
-						    cacheLocationLevel, tags));
-						openmrsLocationService.getLocationsByLevelAndTags(id, cacheLocationTag, tags);
-						logger.info(String.format("completed populating OpenMRS location cache for %s, %s, %s ", id,
-						    cacheLocationLevel, tags));
+		if (contextRefreshedEvent.getApplicationContext().getId().endsWith(APPLICATION_ID + APPLICATION_ID)) {
+		 
+						logger.info("Populating OpenMRS location cache");
 						
-					}
-				});
-			}
+						fetchLocationsHelper.getAllOpenMRSlocations();
+						
+						logger.info("Completed populating OpenMRS location cache");
 		}
 	}
 }
