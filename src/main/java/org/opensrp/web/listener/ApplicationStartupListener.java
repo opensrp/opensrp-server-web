eee8d9231e665c5a85aa8ccc083fa662f85eefa4
Samuel Githengi <samuel.githengi@gmail.com>
2020-07-29 09:48:06 +0300

    populate cache only if enabled via configs

--- src/main/java/org/opensrp/web/listener/ApplicationStartupListener.java
+++ src/main/java/org/opensrp/web/listener/ApplicationStartupListener.java
@@ -4,6 +4,7 @@ import org.opensrp.connector.openmrs.FetchLocationsHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -18,23 +19,27 @@ public class ApplicationStartupListener implements ApplicationListener<ContextRe
 	@Autowired
 	private FetchLocationsHelper fetchLocationsHelper;
 	
+	@Value("#{opensrp['openmrs.location.cache.enabled] ?: false }")
+	private boolean cacheLocationEnabled;
+	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 		logger.info(contextRefreshedEvent.getApplicationContext().getId());
-		if (contextRefreshedEvent.getApplicationContext().getId().endsWith(APPLICATION_ID + APPLICATION_ID)) {
+		if (contextRefreshedEvent.getApplicationContext().getId().endsWith(APPLICATION_ID + APPLICATION_ID)
+		        && cacheLocationEnabled) {
 			
-			
-			new Thread(){
-			    @Override
-				public void run(){
-			    	
-			    	logger.info("Populating OpenMRS location cache");
+			new Thread() {
+				
+				@Override
+				public void run() {
+					
+					logger.info("Populating OpenMRS location cache");
 					
 					fetchLocationsHelper.getAllOpenMRSlocations();
 					
 					logger.info("Completed populating OpenMRS location cache");
-			    }
-			  }.start(); 
+				}
+			}.start();
 			
 		}
 	}
