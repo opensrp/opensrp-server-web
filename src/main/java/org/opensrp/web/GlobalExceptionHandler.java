0b7223bef877ff06c4f00e84c1928051db3fd6bf
Samuel Githengi <samuel.githengi@gmail.com>
2020-09-15 18:15:42 +0300

    add method not found exception on exception handler

--- src/main/java/org/opensrp/web/GlobalExceptionHandler.java
+++ src/main/java/org/opensrp/web/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -45,6 +46,14 @@ public class GlobalExceptionHandler {
 		return buildErrorResponseForBadRequest(HttpStatus.BAD_REQUEST, "");
 	}
 	
+	
+	@ResponseBody
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+	public ResponseDto<?> exceptionHandler(HttpRequestMethodNotSupportedException exception) {
+		logger.error("Method not allowed occurred : ", exception);
+		return buildErrorResponse(HttpStatus.METHOD_NOT_ALLOWED);
+	}
 	
 	@ResponseBody
 	@ExceptionHandler(RuntimeException.class)
