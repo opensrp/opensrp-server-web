9b23d08aad70d45390daeca0f7ecaadc040e7e27
Reham Muzzamil <reham.muzzamil@venturedive.com>
2020-11-02 12:05:39 +0500

    Fix broken build

--- src/main/java/org/opensrp/web/rest/StockResource.java
+++ src/main/java/org/opensrp/web/rest/StockResource.java
@@ -57,6 +57,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.google.gson.Gson;
