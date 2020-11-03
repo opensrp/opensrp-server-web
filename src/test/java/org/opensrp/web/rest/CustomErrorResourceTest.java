e3be42753797d560a0b89d2f83e700b0f417ce1e
Simon Njoroge <snjoroge@ona.io>
2020-11-03 16:13:13 +0300

    Add unit test

---
+++ src/test/java/org/opensrp/web/rest/CustomErrorResourceTest.java
@@ -0,0 +1,47 @@
+package org.opensrp.web.rest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.opensrp.web.GlobalExceptionHandler;
+import org.opensrp.web.rest.it.TestWebContextLoader;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = TestWebContextLoader.class, locations = { "classpath:test-webmvc-config.xml" })
+public class CustomErrorResourceTest {
+
+	private MockMvc mockMvc;
+
+	private String ERROR_ENDPOINT = "/error";
+
+	@InjectMocks
+	private CustomErrorResource customErrorResource;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+
+		mockMvc = MockMvcBuilders.standaloneSetup(customErrorResource)
+				.setControllerAdvice(new GlobalExceptionHandler())
+				.build();
+	}
+
+	@Test
+	public void testErrorEndpointReturnsJsonString() throws Exception {
+		mockMvc.perform(get(ERROR_ENDPOINT)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().json("{\"message\":\"OK\",\"status\":\"200 OK\",\"data\":null,\"success\":true}"));
+	}
+}
