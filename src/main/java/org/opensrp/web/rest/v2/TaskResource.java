c1dc61f965662b783a956934958c44d9bf1dc773
Samuel Githengi <samuel.githengi@gmail.com>
2020-11-12 21:42:44 +0300

    Add a Task v2 API

---
+++ src/main/java/org/opensrp/web/rest/v2/TaskResource.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.opensrp.web.rest.v2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Samuel Githengi created on 11/12/20
+ */
+@Controller
+@RequestMapping(value = "/rest/v2/task")
+public class TaskResource extends org.opensrp.web.rest.TaskResource{
+	
+}
