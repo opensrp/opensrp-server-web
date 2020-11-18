c42343e86b2a216e225cc0048b8932bf7cf71c22
Samuel Githengi <samuel.githengi@gmail.com>
2020-11-18 12:59:59 +0300

    Handle conversion for v1 of the API

---
+++ src/main/java/org/opensrp/web/dto/TaskSyncRequestWrapper.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.opensrp.web.dto;
+
+import static org.opensrp.web.Constants.RETURN_COUNT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Samuel Githengi created on 11/18/20
+ */
+public  class TaskSyncRequestWrapper {
+	
+	@JsonProperty
+	private List<String> plan = new ArrayList<>();
+	
+	@JsonProperty
+	private List<String> group = new ArrayList<>();
+	
+	@JsonProperty
+	private long serverVersion;
+
+	@JsonProperty
+	private String owner;
+	
+	@JsonProperty(RETURN_COUNT)
+	private boolean returnCount;
+
+	public List<String> getPlan() {
+		return plan;
+	}
+	
+	public List<String> getGroup() {
+		return group;
+	}
+	
+	public long getServerVersion() {
+		return serverVersion;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	
+	public boolean isReturnCount() {
+		return returnCount;
+	}
+	
+}
