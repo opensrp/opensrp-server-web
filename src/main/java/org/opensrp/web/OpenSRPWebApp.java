f88fde6d34621e092a53c120bb042c243b8c7c03
Vincent Karuri <vincentmkaruri@gmail.com>
2020-11-26 10:16:52 +0300

    Add basic OpenSRPWebApp spring boot app

---
+++ src/main/java/org/opensrp/web/OpenSRPWebApp.java
@@ -0,0 +1,12 @@
+package org.opensrp.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Vincent Karuri on 26/11/2020
+ */
+
+@SpringBootApplication
+public class OpenSRPWebApp {
+
+}
