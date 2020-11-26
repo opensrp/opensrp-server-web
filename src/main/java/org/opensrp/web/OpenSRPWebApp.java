9761fdab77715f15d6867583290b285a4f250159
Vincent Karuri <vincentmkaruri@gmail.com>
2020-11-26 13:58:50 +0300

    Import application context xml

--- src/main/java/org/opensrp/web/OpenSRPWebApp.java
+++ src/main/java/org/opensrp/web/OpenSRPWebApp.java
@@ -2,12 +2,14 @@ package org.opensrp.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by Vincent Karuri on 26/11/2020
  */
 
 @SpringBootApplication
+@ImportResource({"classpath:spring/applicationContext-opensrp-web.xml"})
 public class OpenSRPWebApp {
 	public static void main(String[] args) {
 		SpringApplication.run(OpenSRPWebApp.class, args);
