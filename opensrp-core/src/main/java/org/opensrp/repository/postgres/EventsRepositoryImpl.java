f5780d864bcd21f3cb3620f7f503b09b4442daea
proshanto <proshanto123@gmail.com>
2020-12-03 13:16:11 +0600

    fix eventType

--- opensrp-core/src/main/java/org/opensrp/repository/postgres/EventsRepositoryImpl.java
+++ opensrp-core/src/main/java/org/opensrp/repository/postgres/EventsRepositoryImpl.java
@@ -71,7 +71,7 @@ public class EventsRepositoryImpl extends CustomBaseRepositoryImpl<Event> implem
 		pgEvent.setProviderId(entity.getProviderId());
 		pgEvent.setBaseEntityId(entity.getBaseEntityId());
 		pgEvent.setFormSubmissionId(entity.getFormSubmissionId());
-		pgEvent.setEventType(entity.getEntityType());
+		pgEvent.setEventType(entity.getEventType());
 		int rowsAffected = eventMapper.insertSelectiveAndSetId(pgEvent);
 		if (rowsAffected < 1 || pgEvent.getId() == null) {
 			return;
@@ -114,7 +114,7 @@ public class EventsRepositoryImpl extends CustomBaseRepositoryImpl<Event> implem
 		pgEvent.setProviderId(entity.getProviderId());
 		pgEvent.setBaseEntityId(entity.getBaseEntityId());
 		pgEvent.setFormSubmissionId(entity.getFormSubmissionId());
-		pgEvent.setEventType(entity.getEntityType());
+		pgEvent.setEventType(entity.getEventType());
 		EventMetadata eventMetadata = createMetadata(entity, id);
 		if (eventMetadata == null) {
 			return;
