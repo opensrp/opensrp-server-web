bb1bfcd61de5dcd49f24af47a0c0af2da16136bf
Vincent Karuri <vincentmkaruri@gmail.com>
2020-12-04 11:34:44 +0300

    Add mapper class scanner

--- src/main/java/org/opensrp/web/OpenSRPWebApp.java
+++ src/main/java/org/opensrp/web/OpenSRPWebApp.java
@@ -1,5 +1,6 @@
 package org.opensrp.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ImportResource;
  */
 
 @SpringBootApplication
+@MapperScan("org.opensrp.repository.postgres.mapper")
 @ComponentScan(basePackages = {"org.opensrp"}, excludeFilters={
 		@ComponentScan.Filter(type= FilterType.REGEX, pattern={
 				"org.opensrp.repository.couch.*",
