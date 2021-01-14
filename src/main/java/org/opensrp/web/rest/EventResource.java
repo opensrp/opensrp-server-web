06d35bbbc76235fb0dc30ad115cce39c00246fb4
Reham Muzzamil <reham.muzzamil@venturedive.com>
2021-01-14 12:20:02 +0500

    Codacy fixes

--- src/main/java/org/opensrp/web/rest/EventResource.java
+++ src/main/java/org/opensrp/web/rest/EventResource.java
@@ -27,7 +27,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
