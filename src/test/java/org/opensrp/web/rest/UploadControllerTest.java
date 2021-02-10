610824629a9d3be93b4085714d9818b8996b1e12
rkodev <43806892+rkodev@users.noreply.github.com>
2021-02-10 17:24:07 +0300

    Update controller test

--- src/test/java/org/opensrp/web/rest/UploadControllerTest.java
+++ src/test/java/org/opensrp/web/rest/UploadControllerTest.java
@@ -50,7 +50,6 @@ import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
