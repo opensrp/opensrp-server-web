649fe1eb608876de55bcdf8b21ed15ef5ea63b93
bennsimon <botieno@ona.io>
2021-03-05 12:09:14 +0300

    fix codacy issue

--- src/test/java/org/opensrp/web/config/SentryConfigurationTest.java
+++ src/test/java/org/opensrp/web/config/SentryConfigurationTest.java
@@ -1,16 +1,15 @@
 package org.opensrp.web.config;
 
 import static org.mockito.Mockito.atMostOnce;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
-import io.sentry.Sentry;
 import org.junit.Before;
 import org.junit.Test;
 import org.powermock.reflect.internal.WhiteboxImpl;
+
+import io.sentry.Sentry;
 
 public class SentryConfigurationTest {
 	
@@ -30,7 +29,7 @@ public class SentryConfigurationTest {
 	
 	@Test
 	public void testInitializeShouldInitializeSentryIfDsnIsNotEmpty() {
-		WhiteboxImpl.setInternalState(sentryConfiguration,"dsn","https://examplePublicKey@o0.ingest.sentry.io/0");
+		WhiteboxImpl.setInternalState(sentryConfiguration, "dsn", "https://examplePublicKey@o0.ingest.sentry.io/0");
 		sentryConfiguration.initialize();
 		verify(sentryConfiguration, atMostOnce()).initializeSentry();
 		Sentry.close();
