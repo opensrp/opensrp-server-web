82b89057f057f632e996d292b74f5271b0d0977e
bennsimon <botieno@ona.io>
2021-03-08 09:38:09 +0300

    fix formatting

--- src/main/java/org/opensrp/web/config/SentryConfiguration.java
+++ src/main/java/org/opensrp/web/config/SentryConfiguration.java
@@ -2,30 +2,29 @@ package org.opensrp.web.config;
 
 import javax.annotation.PostConstruct;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-
-import com.google.common.annotations.VisibleForTesting;
 
 import io.sentry.Sentry;
 
 @Configuration
 public class SentryConfiguration {
-	
+
 	@Value("#{opensrp['sentry.dsn'] ?: ''}")
 	private String dsn;
-	
+
 	@Value("#{opensrp['sentry.release'] ?: ''}")
 	private String release;
-	
+
 	@PostConstruct
 	public void initialize() {
 		if (StringUtils.isNotBlank(dsn)) {
 			initializeSentry();
 		}
 	}
-	
+
 	@VisibleForTesting
 	protected void initializeSentry() {
 		Sentry.init(sentryOptions -> {
