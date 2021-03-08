d9a87ba6a1eb58dfee1a3eeb9a809bbda1b520e6
bennsimon <botieno@ona.io>
2021-03-08 09:10:11 +0300

    update sentry config

--- src/main/java/org/opensrp/web/config/SentryConfiguration.java
+++ src/main/java/org/opensrp/web/config/SentryConfiguration.java
@@ -2,31 +2,30 @@ package org.opensrp.web.config;
 
 import javax.annotation.PostConstruct;
 
-import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import io.sentry.Sentry;
-import io.sentry.spring.EnableSentry;
+import com.google.common.annotations.VisibleForTesting;
 
-@EnableSentry
+import io.sentry.Sentry;
+
 @Configuration
 public class SentryConfiguration {
-
+	
 	@Value("#{opensrp['sentry.dsn'] ?: ''}")
 	private String dsn;
-
+	
 	@Value("#{opensrp['sentry.release'] ?: ''}")
 	private String release;
-
+	
 	@PostConstruct
 	public void initialize() {
 		if (StringUtils.isNotBlank(dsn)) {
 			initializeSentry();
 		}
 	}
-
+	
 	@VisibleForTesting
 	protected void initializeSentry() {
 		Sentry.init(sentryOptions -> {
