4a6bb11329a8efcce2149020579c030ad46d9a27
Reham Muzzamil <reham.muzzamil@venturedive.com>
2021-03-16 19:26:31 +0500

    769 : Update the practitioner resource to add the get practitioners end-point via another practitioners identifier and role code

--- src/main/java/org/opensrp/web/rest/PractitionerResource.java
+++ src/main/java/org/opensrp/web/rest/PractitionerResource.java
@@ -20,11 +20,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 import static org.opensrp.web.Constants.ORDER_BY_FIELD_NAME;
 import static org.opensrp.web.Constants.ORDER_BY_TYPE;
@@ -145,7 +144,14 @@ public class PractitionerResource {
         }
     }
 
-    private PractitionerSearchBean createPractitionerSearchBean(Integer pageNumber, Integer pageSize, String orderByType,
+    @GetMapping(value = "/report-to", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public List<Practitioner> getPractitionersByPractitionerRoleIdentifierAndCode(@RequestParam(value = "practitionerIdentifier") String practitionerIdentifier,
+            @RequestParam(value = "code") String code) {
+
+        return practitionerService.getAssignedPractitionersByIdentifierAndCode(practitionerIdentifier, code);
+    }
+
+	private PractitionerSearchBean createPractitionerSearchBean(Integer pageNumber, Integer pageSize, String orderByType,
             String orderByFieldName) {
 
         BaseSearchBean.OrderByType orderByTypeEnum;
