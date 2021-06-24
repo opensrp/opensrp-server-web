efb042616bfe4f8862a43a146f64647b3045c32e
Elly Kitoto <junkmailstoelly@gmail.com>
2021-06-24 16:40:57 +0300

    Get all practitioner roles count

--- src/main/java/org/opensrp/web/rest/PractitionerRoleResource.java
+++ src/main/java/org/opensrp/web/rest/PractitionerRoleResource.java
@@ -25,11 +25,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import static org.opensrp.web.Constants.ORDER_BY_FIELD_NAME;
 import static org.opensrp.web.Constants.ORDER_BY_TYPE;
 import static org.opensrp.web.Constants.PAGE_NUMBER;
@@ -172,6 +174,16 @@ public class PractitionerRoleResource {
                 .pageSize(pageSize)
                 .orderByType(orderByTypeEnum)
                 .orderByFieldName(fieldName).build();
+    }
+
+    @GetMapping(value = "/count")
+    public ResponseEntity<Long> getAllPractitionerRolesCount() {
+        try {
+            return new ResponseEntity<>(practitionerRoleService.countAllPractitionerRoles(), HttpStatus.OK);
+        } catch (Exception exception) {
+            logger.error("Error getting practitioners role count", exception);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
