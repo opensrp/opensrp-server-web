062af0f0423710592595ffc287d15f38827230f8
Elly Kitoto <junkmailstoelly@gmail.com>
2021-07-17 03:07:20 +0300

    Update RapidPro contact fields after processing contacts

--- src/main/java/org/opensrp/web/listener/RapidProListener.java
+++ src/main/java/org/opensrp/web/listener/RapidProListener.java
@@ -1,7 +1,10 @@
 package org.opensrp.web.listener;
 
 import org.opensrp.service.rapidpro.RapidProService;
+import org.opensrp.service.rapidpro.ZeirRapidProStateService;
+import org.opensrp.util.constants.RapidProConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Service;
@@ -13,14 +16,26 @@ public class RapidProListener {
 
 	private RapidProService rapidProService;
 
+	private ZeirRapidProStateService rapidProStateService;
+
+	@Value("#{opensrp['rapidpro.project']}")
+	private String rapidProProject;
+
 	@Autowired
 	public void setRapidProService(RapidProService rapidProService) {
 		this.rapidProService = rapidProService;
 	}
 
+	@Autowired
+	public void setRapidProStateService(ZeirRapidProStateService rapidProStateService) {
+		this.rapidProStateService = rapidProStateService;
+	}
+
 	public void requestRapidProContacts() {
 		rapidProService.queryContacts(() -> {
-			//To be implemented
+			if (RapidProConstants.RapidProProjects.ZEIR_RAPIDPRO.equalsIgnoreCase(rapidProProject)) {
+				rapidProStateService.updateRapidProContactsFields();
+			}
 		});
 	}
 }
