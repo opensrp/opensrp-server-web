31a973f63fd36b630f4533ee3fb6f0007fb9405c
Elly Kitoto <junkmailstoelly@gmail.com>
2021-07-22 12:31:28 +0300

    Update server-connector version

--- src/main/java/org/opensrp/web/rest/RapidProResource.java
+++ src/main/java/org/opensrp/web/rest/RapidProResource.java
@@ -1,5 +1,9 @@
 package org.opensrp.web.rest;
 
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONObject;
+import org.opensrp.service.rapidpro.ZeirRapidProService;
+import org.opensrp.util.constants.RapidProConstants;
 import org.opensrp.web.Constants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -19,6 +23,11 @@ public class RapidProResource {
 	@Value("#{opensrp['rapidpro.token']}")
 	private String rapidProToken;
 
+	@Value("#{opensrp['rapidpro.project']}")
+	private String rapidProProject;
+
+	private ZeirRapidProService zeirRapidProService;
+
 	@RequestMapping(value = "/callback", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE,
 			MediaType.TEXT_PLAIN_VALUE }, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> callback(HttpServletRequest request, @RequestBody String payload) {
@@ -26,9 +35,13 @@ public class RapidProResource {
 		String apiToken = request.getHeader(Constants.AUTHORIZATION);
 		String[] tokenValues = apiToken.split(" ");
 
-		if (tokenValues.length == 2 && rapidProToken.equalsIgnoreCase(tokenValues[1].trim())) {
-			ResponseEntity<String> responseEntity = new ResponseEntity<>("{}", HttpStatus.ACCEPTED);
-			return responseEntity;
+		if (tokenValues.length == 2 && StringUtils.isNotBlank(rapidProToken) && StringUtils.isNotBlank(rapidProProject)
+				&& rapidProToken.equalsIgnoreCase(tokenValues[1].trim())) {
+			if (RapidProConstants.RapidProProjects.ZEIR_RAPIDPRO.equalsIgnoreCase(rapidProProject)) {
+				JSONObject responseJson = new JSONObject();
+				responseJson.put(RapidProConstants.OPENSRP_ID, "1000122");
+				return new ResponseEntity<>(responseJson.toString(), HttpStatus.OK);
+			}
 		}
 		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
