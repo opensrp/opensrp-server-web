401646898d805edbd5129bf6c524b0b0df1b48ec
Benn Simon <botieno@ona.io>
2021-09-09 18:20:49 +0300

    Add sentry tags config v2.1 (#940)
    * add ghcr domain to docker images
    
    * update ghcr username
    
    * update sentry configuration with tags
    
    * add test
    
    * update configs repo
    
    * update snapshot

--- src/main/java/org/opensrp/web/config/SentryConfiguration.java
+++ src/main/java/org/opensrp/web/config/SentryConfiguration.java
@@ -3,11 +3,16 @@ package org.opensrp.web.config;
 import javax.annotation.PostConstruct;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.gson.Gson;
+import io.sentry.SentryOptions;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import io.sentry.Sentry;
+
+import java.util.Map;
 
 @Configuration
 public class SentryConfiguration {
@@ -17,6 +22,12 @@ public class SentryConfiguration {
 
 	@Value("#{opensrp['sentry.release'] ?: ''}")
 	private String release;
+
+	@Value("#{opensrp['sentry.environment'] ?: ''}")
+	private String environment;
+
+	@Value("#{opensrp['sentry.tags'] ?: '{}'}")
+	private String tags;
 
 	@PostConstruct
 	public void initialize() {
@@ -30,6 +41,25 @@ public class SentryConfiguration {
 		Sentry.init(sentryOptions -> {
 			sentryOptions.setDsn(dsn);
 			sentryOptions.setRelease(release);
+			sentryOptions.setEnvironment(environment);
+			populateTags(sentryOptions);
 		});
+	}
+	@VisibleForTesting
+	protected void populateTags(SentryOptions sentryOptions) {
+		if(StringUtils.isNotBlank(tags)) {
+			Map<String, String> map;
+			try {
+				map = new Gson().fromJson(tags, Map.class);
+				for (Map.Entry<String, String> extraTagsEntry : map.entrySet()) {
+					String key = extraTagsEntry.getKey();
+					if (StringUtils.isNotBlank(key))
+						sentryOptions.setTag(extraTagsEntry.getKey(), extraTagsEntry.getValue());
+				}
+			}
+			catch (Exception e) {
+				LogManager.getLogger().error(e);
+			}
+		}
 	}
 }
--- src/test/java/org/opensrp/web/config/SentryConfigurationTest.java
+++ src/test/java/org/opensrp/web/config/SentryConfigurationTest.java
@@ -1,10 +1,15 @@
 package org.opensrp.web.config;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atMostOnce;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 
+import io.sentry.SentryOptions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -40,4 +45,31 @@ public class SentryConfigurationTest {
 		sentryConfiguration.initialize();
 		verify(sentryConfiguration, atMostOnce()).initializeSentry();
 	}
+
+	@Test(expected = IllegalAccessError.class)
+	public void testPopulateTagsShouldThrowExceptionIfTagsInvalid() {
+		WhiteboxImpl.setInternalState(sentryConfiguration, "tags", "}");
+		SentryOptions sentryOptions = mock(SentryOptions.class);
+		sentryConfiguration.populateTags(sentryOptions);
+		verify(sentryOptions, never()).setTag(anyString(), anyString());
+	}
+
+	@Test
+	public void testPopulateTagsShouldNotAddTagsIfNotPresent() {
+		WhiteboxImpl.setInternalState(sentryConfiguration, "tags", "{}");
+		SentryOptions sentryOptions = mock(SentryOptions.class);
+		sentryConfiguration.populateTags(sentryOptions);
+		verify(sentryOptions, never()).setTag(anyString(), anyString());
+	}
+
+	@Test
+	public void testPopulateTagsShouldAddTagsToSentryOptions() {
+		String releaseName = "release-name";
+		String release = "release-a";
+		WhiteboxImpl.setInternalState(sentryConfiguration, "tags", String.format("{%s:'%s'}", releaseName, release));
+		SentryOptions sentryOptions = mock(SentryOptions.class);
+		sentryConfiguration.populateTags(sentryOptions);
+		verify(sentryOptions, only()).setTag(eq(releaseName), eq(release));
+	}
+
 }
