2144fa30e86b6797c687a3f5221e94d9f4f68e8a
bennsimon <botieno@ona.io>
2021-09-09 17:17:31 +0300

    add test

--- src/main/java/org/opensrp/web/config/SentryConfiguration.java
+++ src/main/java/org/opensrp/web/config/SentryConfiguration.java
@@ -4,6 +4,7 @@ import javax.annotation.PostConstruct;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.gson.Gson;
+import io.sentry.SentryOptions;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,20 +42,24 @@ public class SentryConfiguration {
 			sentryOptions.setDsn(dsn);
 			sentryOptions.setRelease(release);
 			sentryOptions.setEnvironment(environment);
-			if(StringUtils.isNotBlank(tags)) {
-				Map<String, String> map;
-				try {
-					map = new Gson().fromJson(tags, Map.class);
-					for (Map.Entry<String, String> extraTagsEntry : map.entrySet()) {
-						String key = extraTagsEntry.getKey();
-						if (StringUtils.isNotBlank(key))
-							sentryOptions.setTag(extraTagsEntry.getKey(), extraTagsEntry.getValue());
-					}
-				}
-				catch (Exception e) {
-					LogManager.getLogger().error(e);
+			populateTags(sentryOptions);
+		});
+	}
+	@VisibleForTesting
+	protected void populateTags(SentryOptions sentryOptions) {
+		if(StringUtils.isNotBlank(tags)) {
+			Map<String, String> map;
+			try {
+				map = new Gson().fromJson(tags, Map.class);
+				for (Map.Entry<String, String> extraTagsEntry : map.entrySet()) {
+					String key = extraTagsEntry.getKey();
+					if (StringUtils.isNotBlank(key))
+						sentryOptions.setTag(extraTagsEntry.getKey(), extraTagsEntry.getValue());
 				}
 			}
-		});
+			catch (Exception e) {
+				LogManager.getLogger().error(e);
+			}
+		}
 	}
 }
