209403cb3277e1cce29e053755fc89895e883f06
bennsimon <botieno@ona.io>
2021-09-09 16:50:10 +0300

    update sentry configuration with tags

--- src/main/java/org/opensrp/web/config/SentryConfiguration.java
+++ src/main/java/org/opensrp/web/config/SentryConfiguration.java
@@ -3,11 +3,15 @@ package org.opensrp.web.config;
 import javax.annotation.PostConstruct;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.gson.Gson;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import io.sentry.Sentry;
+
+import java.util.Map;
 
 @Configuration
 public class SentryConfiguration {
@@ -20,6 +24,9 @@ public class SentryConfiguration {
 
 	@Value("#{opensrp['sentry.environment'] ?: ''}")
 	private String environment;
+
+	@Value("#{opensrp['sentry.tags'] ?: '{}'}")
+	private String tags;
 
 	@PostConstruct
 	public void initialize() {
@@ -34,6 +41,20 @@ public class SentryConfiguration {
 			sentryOptions.setDsn(dsn);
 			sentryOptions.setRelease(release);
 			sentryOptions.setEnvironment(environment);
+			if(StringUtils.isNotBlank(tags)) {
+				Map<String, String> map;
+				try {
+					map = new Gson().fromJson(tags, Map.class);
+					for (Map.Entry<String, String> extraTagsEntry : map.entrySet()) {
+						String key = extraTagsEntry.getKey();
+						if (StringUtils.isNotBlank(key))
+							sentryOptions.setTag(extraTagsEntry.getKey(), extraTagsEntry.getValue());
+					}
+				}
+				catch (Exception e) {
+					LogManager.getLogger().error(e);
+				}
+			}
 		});
 	}
 }
