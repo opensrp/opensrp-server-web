86046f0f3491a4b5ed0a0e77c91e33908f521ee1
bennsimon <botieno@ona.io>
2021-11-02 15:07:59 +0300

    update test

--- src/test/java/org/opensrp/TestDatabaseConfig.java
+++ src/test/java/org/opensrp/TestDatabaseConfig.java
@@ -2,7 +2,6 @@ package org.opensrp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.mockito.Mockito.mock;
@@ -13,10 +12,5 @@ public class TestDatabaseConfig {
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return mock(JdbcTemplate.class);
-    }
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return mock(RedisConnectionFactory.class);
     }
 }
