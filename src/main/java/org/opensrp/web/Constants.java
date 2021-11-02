bb2add6b9502e376f0c0aa0c8e457399ef344df4
bennsimon <botieno@ona.io>
2021-11-02 09:59:23 +0300

    add health endpoint

--- src/main/java/org/opensrp/web/Constants.java
+++ src/main/java/org/opensrp/web/Constants.java
@@ -2,11 +2,11 @@ package org.opensrp.web;
 
 public interface Constants {
     String LIMIT = "limit";
-    
+
     int DEFAULT_LIMIT = 25;
-    
+
     int DEFAULT_GET_ALL_IDS_LIMIT = 5000;
-    
+
     String WITH_FAMILY_EVENTS = "withFamilyEvents";
     String BASE_ENTITY_IDS = "baseEntityIds";
     String CLIENTS = "clients";
@@ -17,35 +17,35 @@ public interface Constants {
     String ERROR_OCCURRED = "Error occurred";
     String FAILURE = "Failure occurred";
     String DEFAULT_EXCEPTION_HANDLER_MESSAGE = "The server encountered an error processing the request.";
-    
 
-	String RETURN_COUNT = "return_count";
-	
-	String TOTAL_RECORDS = "total_records";
-	
-	String PAGE_NUMBER = "pageNumber";
-	
-	String PAGE_SIZE = "pageSize";
-	
-	String ORDER_BY_TYPE = "orderByType";
-	
-	String ORDER_BY_FIELD_NAME = "orderByFieldName";
-	
-	String LOCATIONS = "locations";
-	
+
+    String RETURN_COUNT = "return_count";
+
+    String TOTAL_RECORDS = "total_records";
+
+    String PAGE_NUMBER = "pageNumber";
+
+    String PAGE_SIZE = "pageSize";
+
+    String ORDER_BY_TYPE = "orderByType";
+
+    String ORDER_BY_FIELD_NAME = "orderByFieldName";
+
+    String LOCATIONS = "locations";
+
     interface ContentType {
         String APPLICATION_YAML = "application/x-yaml";
         String TEXT_YAML = "text/yaml";
     }
-    
+
     interface RestEndpointUrls {
         String SETTINGS_V2_URL = "/rest/v2/settings";
     }
-    
+
     interface RestPartVariables {
         String ID = "id";
     }
- 
+
     interface EndpointParam {
         String APP_ID = "app_id";
         String STRICT = "strict";
@@ -53,7 +53,7 @@ public interface Constants {
         String IS_DRAFT= "is_draft";
         String IS_JSON_VALIDATOR= "is_json_validator";
     }
-    
+
     interface DefaultEndpointParam {
         String FALSE = "false";
     }
@@ -64,5 +64,16 @@ public interface Constants {
             String FIELDS = "fields";
             String KEY = "key";
         }
+    }
+
+    interface HealthIndicator {
+        String PROBLEMS = "problems";
+        String SERVICES = "services";
+        String STATUS = "status";
+        String INDICATOR = "indicator";
+        String EXCEPTION = "exception";
+        String TIME = "time";
+        String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+        String VERSION = "version";
     }
 }
---
+++ src/main/java/org/opensrp/web/controller/HealthController.java
@@ -0,0 +1,26 @@
+package org.opensrp.web.controller;
+
+import org.opensrp.web.Constants;
+import org.opensrp.web.service.HealthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @Autowired
+    private HealthService healthService;
+
+    @GetMapping
+    public ResponseEntity<ModelMap> index() {
+        ModelMap modelMap = healthService.aggregateHealthCheck();
+        ModelMap problemsModelMap = (ModelMap) modelMap.get(Constants.HealthIndicator.PROBLEMS);
+        return ResponseEntity.status(problemsModelMap.isEmpty() ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR).body(modelMap);
+    }
+}
