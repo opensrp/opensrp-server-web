0a3ad8cb75449f09560aa3df5982689aac8f07a7
Benn Simon <botieno@ona.io>
2022-02-10 13:51:01 +0300

    Dependencies Update (master) (#1063)
    * update dependencies to fix security vulnerabilities
    
    * update swaggerConfig test
    
    * fix organizationResourceTests
    
    * update DiskSpaceMetrics package

--- src/test/java/org/opensrp/web/rest/OrganizationResourceTest.java
+++ src/test/java/org/opensrp/web/rest/OrganizationResourceTest.java
@@ -113,7 +113,7 @@ public class OrganizationResourceTest {
 
 	private String BASE_URL = "/rest/organization/";
 
-	private String organizationJSON = "{\"identifier\":\"801874c0-d963-11e9-8a34-2a2ae2dbcce4\",\"active\":true,\"name\":\"B Team\",\"partOf\":1123,\"type\":{\"coding\":[{\"system\":\"http://terminology.hl7.org/CodeSystem/organization-type\",\"code\":\"team\",\"display\":\"Team\"}]}}";
+	private String organizationJSON = "{\"identifier\":\"801874c0-d963-11e9-8a34-2a2ae2dbcce4\",\"active\":true,\"name\":\"B Team\",\"partOf\":1123,\"type\":{\"coding\":[{\"system\":\"http://terminology.hl7.org/CodeSystem/organization-type\",\"code\":\"team\",\"display\":\"Team\"}]},\"serverVersion\":0}";
 
 	private ObjectMapper objectMapper;
 
