6fbfff11ed054baba27c03b4c3b2eced159665c7
Simon Njoroge <snjoroge@ona.io>
2022-08-02 11:23:41 +0300

    Clean up logged client data during sync

--- src/main/java/org/opensrp/web/rest/EventResource.java
+++ src/main/java/org/opensrp/web/rest/EventResource.java
@@ -509,7 +509,7 @@ public class EventResource extends RestResource<Event> {
 						clientService.addorUpdate(client);
 					}
 					catch (Exception e) {
-						logger.error("[SYNC_INFO] Sync failed for client {} - {}", client.getBaseEntityId(), gson.toJson(client), e);
+						logger.error("[SYNC_INFO] Sync failed for client {}; identifiers: {}", client.getBaseEntityId(), gson.toJson(client.getIdentifiers()), e);
 						failedClientsIds.add(client.getBaseEntityId());
 					}
 				}
@@ -532,11 +532,10 @@ public class EventResource extends RestResource<Event> {
 					}
 					catch (Exception e) {
 						logger.error(
-								"[SYNC_INFO] Sync failed for event {}; type: {} for client {} - {}",
+								"[SYNC_INFO] Sync failed for event {}; type: {} for client {}",
 								event.getFormSubmissionId(),
 								event.getEventType(),
 								event.getBaseEntityId(),
-								gson.toJson(event),
 								e
 						);
 						failedEventIds.add(event.getFormSubmissionId());
