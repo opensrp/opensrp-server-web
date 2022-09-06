42d72f321898837a254143cf08ee86ff1f5747a0
hilpitome <hilpitome@gmail.com>
2022-09-06 13:16:30 +0300

    test CachedBodyServletInputStream ReadListener methods

--- src/test/java/org/opensrp/web/CachedBodyServletInputStreamTest.java
+++ src/test/java/org/opensrp/web/CachedBodyServletInputStreamTest.java
@@ -1,19 +1,21 @@
 package org.opensrp.web;
 
-import junit.framework.TestCase;
+import lombok.SneakyThrows;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import javax.servlet.ReadListener;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-public class CachedBodyServletInputStreamTest extends TestCase {
+public class CachedBodyServletInputStreamTest {
 
     @Test
     public void testIsFinishedShouldReturnTrueWhenReadIsDone() throws IOException {
         String content = "This is a test request";
         CachedBodyServletInputStream cachedBodyServletInputStream  = new CachedBodyServletInputStream(content.getBytes(StandardCharsets.UTF_8));
-        assertFalse(cachedBodyServletInputStream.isFinished);
+        Assert.assertFalse(cachedBodyServletInputStream.isFinished);
         int data;
         while(( data = cachedBodyServletInputStream.read() )!= -1){
             // do nothing
@@ -30,6 +32,25 @@ public class CachedBodyServletInputStreamTest extends TestCase {
         Assert.assertTrue(cachedBodyServletInputStream.isReady());
     }
 
+    @SneakyThrows
+    @Test
+    public void testSetReadListenerShouldCallListenerOnDataAvailable(){
+        String content = "test";
+        CachedBodyServletInputStream cachedBodyServletInputStream = new CachedBodyServletInputStream(content.getBytes(StandardCharsets.UTF_8));
+        ReadListener listener  = Mockito.mock(ReadListener.class);
+        cachedBodyServletInputStream.setReadListener(listener);
+        Mockito.verify(listener).onDataAvailable();
+    }
 
+    @SneakyThrows
+    @Test
+    public void testSetReadListenerShouldCallListenerOnErrorWhenIOExceptionIsThrown(){
+        String content = "test";
+        CachedBodyServletInputStream cachedBodyServletInputStream = new CachedBodyServletInputStream(content.getBytes(StandardCharsets.UTF_8));
+        ReadListener listener  = Mockito.mock(ReadListener.class);
+        Mockito.doThrow(new IOException()).when(listener).onDataAvailable();
+        cachedBodyServletInputStream.setReadListener(listener);
+        Mockito.verify(listener).onError(Mockito.any());
+    }
 
 }
