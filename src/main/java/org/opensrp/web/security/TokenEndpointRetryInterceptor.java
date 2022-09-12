1f681756df413566d58a8b709cc5a8103b6fd7ce
hilpitome <hilpitome@gmail.com>
2022-09-12 09:21:14 +0300

    add interceptor to fix dup authentication_id issue

---
+++ src/main/java/org/opensrp/web/security/TokenEndpointRetryInterceptor.java
@@ -0,0 +1,28 @@
+package org.opensrp.web.security;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TokenEndpointRetryInterceptor {
+    private static final int MAX_RETRIES = 4;
+
+    @Around("execution(* org.springframework.security.oauth2.provider.endpoint.TokenEndpoint.*(..))")
+    public Object execute (ProceedingJoinPoint aJoinPoint) throws Throwable {
+        int tts = 1000;
+        for (int i=0; i<MAX_RETRIES; i++) {
+            try {
+                return aJoinPoint.proceed();
+            } catch (DuplicateKeyException e) {
+                Thread.sleep(tts);
+                tts=tts*2;
+            }
+        }
+        throw new IllegalStateException("Could not execute: " + aJoinPoint.getSignature().getName());
+    }
+
+}
