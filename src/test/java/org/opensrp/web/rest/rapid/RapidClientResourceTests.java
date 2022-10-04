55c27fd6158a202d0a53cfcf651de16a69e745aa
SebaMutuku <36365043+SebaMutuku@users.noreply.github.com>
2022-10-04 14:11:49 +0300

    testEligibleVaccines amendment

--- src/test/java/org/opensrp/web/rest/rapid/RapidClientResourceTests.java
+++ src/test/java/org/opensrp/web/rest/rapid/RapidClientResourceTests.java
@@ -3,22 +3,24 @@ package org.opensrp.web.rest.rapid;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.opensrp.web.rest.it.BaseResourceTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.opensrp.web.rest.it.TestWebContextLoader;
+import org.powermock.reflect.Whitebox;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 
-@RunWith(SpringRunner.class)
-public class RapidClientResourceTests extends BaseResourceTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = TestWebContextLoader.class, locations = { "classpath*:context.xml", })
+@ActiveProfiles(profiles = { "jedis", "postgres", "basic_auth", "rabbitmq" })
+public class RapidClientResourceTests {
 
 	@Test
 	public void testEligibleVaccines() throws Exception {
 		int age = 3;
-		Method method = RapidClientResource.class.getDeclaredMethod("eligibleVaccines", Integer.class);
-		ArrayList<String> response = (ArrayList<String>) method.invoke("eligibleVaccines", age);
+		ArrayList<String> response = Whitebox.invokeMethod(RapidClientResource.class, "eligibleVaccines", age);
 		Assert.assertNotNull(response.size());
-
 	}
 
 }
