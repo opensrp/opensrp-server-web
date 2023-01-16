a5b90a54155766374b90aeaa85c2e6ee1ee2e477
hilpitome <hilpitome@gmail.com>
2023-01-16 13:07:28 +0300

    codacy fixe

--- src/main/java/org/opensrp/web/utils/Utils.java
+++ src/main/java/org/opensrp/web/utils/Utils.java
@@ -5,7 +5,6 @@ package org.opensrp.web.utils;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
