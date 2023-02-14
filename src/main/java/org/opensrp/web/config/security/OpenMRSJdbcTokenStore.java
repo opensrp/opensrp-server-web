bea4049860e839cfd734e6a004feeb5723ee6c17
hilpitome <hilpitome@gmail.com>
2023-02-14 12:48:46 +0300

    apply openMRS formatter

--- src/main/java/org/opensrp/web/config/security/OpenMRSJdbcTokenStore.java
+++ src/main/java/org/opensrp/web/config/security/OpenMRSJdbcTokenStore.java
@@ -10,32 +10,35 @@ import javax.sql.DataSource;
 import java.lang.reflect.Field;
 
 public class OpenMRSJdbcTokenStore extends JdbcTokenStore {
-    public OpenMRSJdbcTokenStore(DataSource dataSource) {
-        super(dataSource);
-    }
-
-    @Override
-    public void storeAccessToken(final OAuth2AccessToken token, final OAuth2Authentication authentication) {
-        try {
-
-            // use reflection to access private fields in superclass
-            Field jdbcTemplateField = JdbcTokenStore.class.getDeclaredField("jdbcTemplate");
-            jdbcTemplateField.setAccessible(true);
-            JdbcTemplate template = (JdbcTemplate) jdbcTemplateField.get(null);
-
-            Field authKeyGeneratorField = JdbcTokenStore.class.getDeclaredField("authenticationKeyGenerator");
-            jdbcTemplateField.setAccessible(true);
-            AuthenticationKeyGenerator authKeyGenerator = (AuthenticationKeyGenerator) authKeyGeneratorField.get(null);
-
-            final String key = authKeyGenerator.extractKey(authentication);
-            template.update("delete from oauth_access_token where authentication_id = ?", key);
-
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } finally {
-            super.storeAccessToken(token, authentication);
-        }
-
-    }
-
+	
+	public OpenMRSJdbcTokenStore(DataSource dataSource) {
+		super(dataSource);
+	}
+	
+	@Override
+	public void storeAccessToken(final OAuth2AccessToken token, final OAuth2Authentication authentication) {
+		try {
+			
+			// use reflection to access private fields in superclass
+			Field jdbcTemplateField = JdbcTokenStore.class.getDeclaredField("jdbcTemplate");
+			jdbcTemplateField.setAccessible(true);
+			JdbcTemplate template = (JdbcTemplate) jdbcTemplateField.get(null);
+			
+			Field authKeyGeneratorField = JdbcTokenStore.class.getDeclaredField("authenticationKeyGenerator");
+			jdbcTemplateField.setAccessible(true);
+			AuthenticationKeyGenerator authKeyGenerator = (AuthenticationKeyGenerator) authKeyGeneratorField.get(null);
+			
+			final String key = authKeyGenerator.extractKey(authentication);
+			template.update("delete from oauth_access_token where authentication_id = ?", key);
+			
+		}
+		catch (NoSuchFieldException | IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			super.storeAccessToken(token, authentication);
+		}
+		
+	}
+	
 }
